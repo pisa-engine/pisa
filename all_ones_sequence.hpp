df225f850f654f8ce8535c0af1aad06c00d78f64
Giuseppe Ottaviano <giuott@gmail.com>
2015-06-21 20:11:07 -0700

    Singleton blocks

--- all_ones_sequence.hpp
+++ all_ones_sequence.hpp
@@ -10,16 +10,17 @@ namespace ds2i {
         inline static uint64_t
         bitsize(global_parameters const& /* params */, uint64_t universe, uint64_t n)
         {
-            return (universe == n) ? 0 : uint64_t(-1);
+            return (universe == n || n == 1) ? 0 : uint64_t(-1);
         }
 
         template <typename Iterator>
         static void write(succinct::bit_vector_builder&,
-                          Iterator,
+                          Iterator begin,
                           uint64_t universe, uint64_t n,
                           global_parameters const&)
         {
-            assert(universe == n); (void)universe; (void)n;
+            assert(universe == n || n == 1); (void)universe; (void)n;
+            assert(*std::advance(begin, n - 1) == universe - 1); (void)begin;
         }
 
         class enumerator {
@@ -30,35 +31,40 @@ namespace ds2i {
             enumerator(succinct::bit_vector const&, uint64_t,
                        uint64_t universe, uint64_t n,
                        global_parameters const&)
-                : m_universe(universe)
+                : m_n(n)
+                , m_universe(universe)
                 , m_position(size())
             {
-                assert(universe == n); (void)n;
+                assert(universe == n || n == 1); (void)n;
             }
 
             value_type move(uint64_t position)
             {
                 assert(position <= size());
                 m_position = position;
-                return value_type(m_position, m_position);
+                return value();
             }
 
             value_type next_geq(uint64_t lower_bound)
             {
-                assert(lower_bound <= size());
-                m_position = lower_bound;
-                return value_type(m_position, m_position);
+                assert(lower_bound <= m_universe);
+                if (m_n == 1) {
+                    m_position = 0;
+                } else {
+                    m_position = lower_bound;
+                }
+                return value();
             }
 
             value_type next()
             {
                 m_position += 1;
-                return value_type(m_position, m_position);
+                return value();
             }
 
             uint64_t size() const
             {
-                return m_universe;
+                return m_n;
             }
 
             uint64_t prev_value() const
@@ -66,10 +72,22 @@ namespace ds2i {
                 if (m_position == 0) {
                     return 0;
                 }
+                if (m_n == 1) {
+                    return m_universe - 1;
+                }
                 return m_position - 1;
             }
 
         private:
+            value_type value() const {
+                if (m_n == 1) {
+                    return value_type(m_position, m_universe - 1);
+                } else {
+                    return value_type(m_position, m_position);
+                }
+            }
+
+            uint64_t m_n;
             uint64_t m_universe;
             uint64_t m_position;
         };
