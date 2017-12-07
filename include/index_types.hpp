9829b4ecea4e150c1badb36d00869b9f6d871cbe
Antonio Mallia <me@antoniomallia.it>
2017-12-07 01:13:07 +0100

    Removed typedefs if favour of aliases

--- include/index_types.hpp
+++ include/index_types.hpp
@@ -1,41 +1,40 @@
 #pragma once
 
+#include <boost/preprocessor/cat.hpp>
 #include <boost/preprocessor/seq/for_each.hpp>
 #include <boost/preprocessor/stringize.hpp>
-#include <boost/preprocessor/cat.hpp>
 
-#include "freq_index.hpp"
-#include "positive_sequence.hpp"
-#include "partitioned_sequence.hpp"
-#include "uniform_partitioned_sequence.hpp"
 #include "binary_freq_collection.hpp"
-#include "block_freq_index.hpp"
 #include "block_codecs.hpp"
+#include "block_freq_index.hpp"
+#include "freq_index.hpp"
 #include "mixed_block.hpp"
+#include "partitioned_sequence.hpp"
+#include "positive_sequence.hpp"
+#include "uniform_partitioned_sequence.hpp"
 
 namespace ds2i {
+using ef_index =
+    freq_index<compact_elias_fano, positive_sequence<strict_elias_fano>>;
 
-    typedef freq_index<compact_elias_fano,
-                       positive_sequence<strict_elias_fano>> ef_index;
+using single_index = freq_index<indexed_sequence, positive_sequence<>>;
 
-    typedef freq_index<indexed_sequence,
-                       positive_sequence<>> single_index;
+using uniform_index = freq_index<
+    uniform_partitioned_sequence<>,
+    positive_sequence<uniform_partitioned_sequence<strict_sequence>>>;
 
-    typedef freq_index<
-        uniform_partitioned_sequence<>,
-        positive_sequence<uniform_partitioned_sequence<strict_sequence>>
-        > uniform_index;
+using opt_index =
+    freq_index<partitioned_sequence<>,
+               positive_sequence<partitioned_sequence<strict_sequence>>>;
 
-    typedef freq_index<
-        partitioned_sequence<>,
-        positive_sequence<partitioned_sequence<strict_sequence>>
-        > opt_index;
+using block_optpfor_index = block_freq_index<ds2i::optpfor_block>;
+using block_varint_index = block_freq_index<ds2i::varint_G8IU_block>;
+using block_interpolative_index = block_freq_index<ds2i::interpolative_block>;
+using block_mixed_index = block_freq_index<ds2i::mixed_block>;
+} // namespace ds2i
 
-    typedef block_freq_index<ds2i::optpfor_block> block_optpfor_index;
-    typedef block_freq_index<ds2i::varint_G8IU_block> block_varint_index;
-    typedef block_freq_index<ds2i::interpolative_block> block_interpolative_index;
-    typedef block_freq_index<ds2i::mixed_block> block_mixed_index;
-}
-
-#define DS2I_INDEX_TYPES (ef)(single)(uniform)(opt)(block_optpfor)(block_varint)(block_interpolative)(block_mixed)
-#define DS2I_BLOCK_INDEX_TYPES (block_optpfor)(block_varint)(block_interpolative)(block_mixed)
+#define DS2I_INDEX_TYPES                                     \
+    (ef)(single)(uniform)(opt)(block_optpfor)(block_varint)( \
+        block_interpolative)(block_mixed)
+#define DS2I_BLOCK_INDEX_TYPES \
+    (block_optpfor)(block_varint)(block_interpolative)(block_mixed)
