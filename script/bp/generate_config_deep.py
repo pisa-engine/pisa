b35e31e068f9d7025f032d84f0104c3fdc071541
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2019-01-05 09:08:07 -0500

    Scripts to generate net BP

---
+++ script/bp/generate_config_deep.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+
+def make_net(left, right, iterations, level, minsize, out):
+    mid = (left + right) // 2
+    q1 = (left + mid) // 2
+    q3 = (mid + right) // 2
+
+    print(level, iterations, left, mid, mid, right, file=out)
+    print(level + 1, iterations, left, q1, q1, mid, file=out)
+    print(level + 1, iterations, mid, q3, q3, right, file=out)
+    print(level + 2, iterations, left, q1, mid, q3, file=out)
+    print(level + 2, iterations, q1, mid, q3, right, file=out)
+    print(level + 3, iterations, left, q1, q1, mid, file=out)
+    print(level + 3, iterations, mid, q3, q3, right, file=out)
+    print(level + 4, iterations, left, q1, q3, right, file=out)
+    print(level + 4, iterations, q1, mid, mid, q3, file=out)
+
+    if q1 - left > minsize:
+        make_net(left, mid, iterations, level + 5, minsize, out)
+        make_net(mid, right, iterations, level + 5, minsize, out)
+
+def generate(ndoc, out):
+    make_net(0, ndoc, 10, 0, 20, out)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate BP config file.')
+    parser.add_argument('ndoc', type=int,
+                        help='Number of document in collection.')
+    parser.add_argument('-o', '--output',
+                        type=argparse.FileType('w'), default=sys.stdout,
+                        help='Ouptut config file (if absent, write to stdout)')
+    args = parser.parse_args()
+    generate(args.ndoc, args.output)
