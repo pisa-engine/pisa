a4b076d12a93ef1eef5d04f245d54a56aba818b9
Antonio Mallia <me@antoniomallia.it>
2019-01-21 19:15:12 +0100

    Added wand data range-based

---
+++ include/pisa/wand_data_range.hpp
@@ -0,0 +1,110 @@
+#pragma once
+
+#include "spdlog/spdlog.h"
+
+#include "succinct/mappable_vector.hpp"
+
+#include "binary_freq_collection.hpp"
+#include "scorer/bm25.hpp"
+#include "util/util.hpp"
+#include "wand_utils.hpp"
+
+namespace pisa {
+
+template <size_t range_size = 1024, typename Scorer = bm25>
+class wand_data_range {
+   public:
+    wand_data_range() {}
+
+    class builder {
+       public:
+        builder(partition_type                type,
+                binary_freq_collection const &coll,
+                global_parameters const &     params) {
+            (void)params;
+            this->type = type;
+            blocks_num = ceil_div(coll.num_docs(), range_size);
+            auto posting_lists = std::distance(coll.begin(), coll.end());
+            spdlog::info("Storing max weight for each list and for each block...");
+            spdlog::info("Range size: {}. Number of docs: {}. Blocks per posting list: {}. Posting lists: {}."
+                , range_size, coll.num_docs(), blocks_num, posting_lists);
+            total_elements = 0;
+        }
+
+        float add_sequence(binary_freq_collection::sequence const &seq,
+                           binary_freq_collection const &          coll,
+                           std::vector<float> const &              norm_lens) {
+            float max_score =0.0f;
+            if (seq.docs.size() > configuration::get().threshold_wand_list) {
+                std::vector<float> b_max(blocks_num, 0.0f);
+                for (auto i = 0; i < seq.docs.size(); ++i) {
+                    uint64_t docid = *(seq.docs.begin() + i);
+                    uint64_t freq = *(seq.freqs.begin() + i);
+                    float score = Scorer::doc_term_weight(freq, norm_lens[docid]);
+                    max_score = std::max(max_score, score);
+                    float bm = block_max_term_weight[docid/range_size];
+                    bm = std::max(bm, score);
+                }
+                block_max_term_weight.insert(
+                    block_max_term_weight.end(), b_max.begin(), b_max.end());
+                total_elements += seq.docs.size();
+            }
+            return max_score;
+        }
+
+        void build(wand_data_range &wdata) {
+            wdata.m_blocks_num = blocks_num;
+            wdata.m_block_max_term_weight.steal(block_max_term_weight);
+            spdlog::info("number of elements / number of blocks: {}",
+                         static_cast<float>(total_elements) / static_cast<float>(wdata.m_block_max_term_weight.size()));
+        }
+
+        uint64_t           blocks_num;
+        partition_type     type;
+        uint64_t           total_elements;
+        std::vector<float> block_max_term_weight;
+    };
+
+    class enumerator {
+        friend class wand_data_range;
+
+       public:
+        enumerator(uint32_t                              _block_start,
+                   mapper::mappable_vector<float> const &block_max_term_weight)
+            : cur_pos(0),
+              block_start(_block_start),
+              m_block_max_term_weight(block_max_term_weight) {}
+
+        void DS2I_NOINLINE next_geq(uint64_t lower_bound) {
+            cur_pos = lower_bound/range_size;
+        }
+
+        float DS2I_FLATTEN_FUNC score() const {
+            return m_block_max_term_weight[block_start + cur_pos];
+        }
+
+        uint64_t DS2I_FLATTEN_FUNC docid() const { return (cur_pos+1) * range_size; }
+
+       private:
+        uint64_t                              cur_pos;
+        uint64_t                              block_start;
+        mapper::mappable_vector<float> const &m_block_max_term_weight;
+    };
+
+    enumerator get_enum(uint32_t i) const {
+        return enumerator(
+            i * m_blocks_num, m_block_max_term_weight);
+    }
+
+    template <typename Visitor>
+    void map(Visitor &visit) {
+        visit(m_blocks_num, "m_blocks_num")
+             (m_block_max_term_weight, "m_block_max_term_weight");
+    }
+
+   private:
+    uint64_t m_blocks_num;
+    mapper::mappable_vector<float> m_block_max_term_weight;
+};
+
+} // namespace pisa
--- src/create_wand_data.cpp
+++ src/create_wand_data.cpp
@@ -10,6 +10,7 @@
 #include "wand_data.hpp"
 #include "wand_data_compressed.hpp"
 #include "wand_data_raw.hpp"
+#include "wand_data_range.hpp"
 
 #include "CLI/CLI.hpp"
 
@@ -20,12 +21,15 @@ int main(int argc, const char **argv) {
     std::string output_filename;
     bool        variable_block = false;
     bool        compress       = false;
+    bool        range          = false;
 
     CLI::App app{"create_wand_data - a tool for creating additional data for query processing."};
     app.add_option("-c,--collection", input_basename, "Collection basename")->required();
     app.add_option("-o,--output", output_filename, "Output filename")->required();
     app.add_flag("--variable-block", variable_block, "Variable length blocks");
     app.add_flag("--compress", compress, "Compress additional data");
+    app.add_flag("--range", range, "Create docid-range based data");
+
     CLI11_PARSE(app, argc, argv);
 
     partition_type p_type =
@@ -40,6 +44,10 @@ int main(int argc, const char **argv) {
 
     if (compress) {
         wand_data<bm25, wand_data_compressed<bm25, uniform_score_compressor>> wdata(
+            sizes_coll.begin()->begin(), coll.num_docs(), coll, p_type);
+        mapper::freeze(wdata, output_filename.c_str());
+    } else if(range) {
+        wand_data<bm25, wand_data_range<1024, bm25>> wdata(
             sizes_coll.begin()->begin(), coll.num_docs(), coll, p_type);
         mapper::freeze(wdata, output_filename.c_str());
     } else {
