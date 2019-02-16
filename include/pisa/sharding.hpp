bb8f69948a5df15598e3440493aca85e3cf6290f
Michal Siedlaczek <michal.siedlaczek@gmail.com>
2019-02-16 00:09:47 +0000

    Small refactoring

--- include/pisa/sharding.hpp
+++ include/pisa/sharding.hpp
@@ -9,8 +9,13 @@
 #include <pstl/algorithm>
 #include <pstl/execution>
 #include <pstl/numeric>
+#include <range/v3/action/shuffle.hpp>
+#include <range/v3/action/sort.hpp>
+#include <range/v3/algorithm/for_each.hpp>
 #include <range/v3/view/chunk.hpp>
 #include <range/v3/view/iota.hpp>
+#include <range/v3/view/take.hpp>
+#include <range/v3/view/transform.hpp>
 #include <range/v3/view/zip.hpp>
 #include <spdlog/spdlog.h>
 #include <tbb/task_scheduler_init.h>
@@ -22,26 +27,28 @@
 
 namespace pisa {
 
+using pisa::literals::operator""_d;
+
 auto create_random_mapping(int document_count,
                            int shard_count,
                            std::optional<std::uint64_t> seed = std::nullopt)
     -> Vector<Document_Id, Shard_Id>
 {
-    namespace rv = ranges::view;
-    std::vector<Document_Id> documents(document_count);
-    std::iota(documents.begin(), documents.end(), Document_Id{});
     std::random_device rd;
     std::mt19937 g(seed.value_or(rd()));
-    std::shuffle(documents.begin(), documents.end(), g);
-    auto shard_size = ceil_div(document_count, shard_count);
     Vector<Document_Id, Shard_Id> mapping(document_count);
-    for (auto &&[shard_id, shard] :
-         rv::zip(rv::iota(Shard_Id{0u}, Shard_Id{shard_count}), rv::chunk(documents, shard_size)))
-    {
-        for (auto document : shard) {
-            mapping[document] = shard_id;
-        }
-    }
+    auto shard_size = ceil_div(document_count, shard_count);
+    auto documents = ranges::view::iota(0_d, Document_Id{document_count}) | ranges::to_vector
+                     | ranges::action::shuffle(g);
+
+    ranges::for_each(ranges::view::zip(ranges::view::iota(Shard_Id{0u}, Shard_Id{shard_count}),
+                                       ranges::view::chunk(documents, shard_size)),
+                     [&](auto &&entry) {
+                         auto &&[shard_id, shard_documents] = entry;
+                         for (auto document : shard_documents) {
+                             mapping[document] = shard_id;
+                         }
+                     });
     return mapping;
 }
 
@@ -61,7 +68,7 @@ auto build_shard(std::string const &input_basename,
                  Vector<Term_Id, std::string> const &terms)
 {
     auto fwd = binary_collection(input_basename.c_str());
-    std::sort(documents.begin(), documents.end());
+    documents |= ranges::action::sort;
 
     std::ofstream os(fmt::format("{}.{:03d}", output_basename, shard_id.as_int()));
 
@@ -73,7 +80,8 @@ auto build_shard(std::string const &input_basename,
         Document_Id current_document{0};
 
         for (auto fwd_iter = ++fwd.begin(); fwd_iter != fwd.end();
-             std::advance(fwd_iter, 1), current_document += 1) {
+             std::advance(fwd_iter, 1), current_document += 1)
+        {
             if (current_document != *docid_iter) {
                 continue;
             }
@@ -138,11 +146,10 @@ auto partition_fwd_index(std::string const &input_basename,
     spdlog::info("Partitioning documents");
     std::vector<std::pair<Shard_Id, std::vector<Document_Id>>> shard_documents;
     {
-        auto mapped_pairs = ranges::to_vector(mapping.entries());
-        std::sort(std::execution::par_unseq,
-                  mapped_pairs.begin(),
-                  mapped_pairs.end(),
-                  [](auto const &lhs, auto const &rhs) { return lhs.second < rhs.second; });
+        auto mapped_pairs = mapping.entries() | ranges::to_vector |
+                            ranges::action::sort([](auto const &lhs, auto const &rhs) {
+                                return lhs.second < rhs.second;
+                            });
         auto pos = mapped_pairs.begin();
         while (pos != mapped_pairs.end()) {
             shard_documents.emplace_back(pos->second, std::vector<Document_Id>{});
@@ -159,7 +166,7 @@ auto partition_fwd_index(std::string const &input_basename,
     }
 
     std::for_each(
-        //std::execution::par_unseq,
+        std::execution::par_unseq,
         shard_documents.begin(),
         shard_documents.end(),
         [&](auto &&entry) {
