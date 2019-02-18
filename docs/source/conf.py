428e1d794e5a0e5da28913481dca18a9ae50667a
Antonio Mallia <me@antoniomallia.it>
2019-02-18 13:46:38 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,7 @@ release = ''
 extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
+    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,8 +50,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
