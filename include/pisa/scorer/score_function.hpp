387635de7975f92bceedd203048a045801b676b0
Antonio Mallia <me@antoniomallia.it>
2019-02-18 00:04:50 -0500

    clang format

--- include/pisa/scorer/score_function.hpp
+++ include/pisa/scorer/score_function.hpp
@@ -1,11 +1,12 @@
 #pragma once
 #include <cstdint>
+
 template <typename Scorer, typename Wand>
 struct Score_Function {
     float query_weight;
     const Wand&  wdata;
 
-    [[nodiscard]] auto operator()(uint32_t doc, uint32_t freq) const -> float {
+    [[nodiscard]]  float operator()(uint32_t doc, uint32_t freq) const {
         return query_weight * Scorer::doc_term_weight(freq, wdata.norm_len(doc));
     }
 };
