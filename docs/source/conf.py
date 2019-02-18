2460fe23badec133b7a31e63f36727cb6c2429b6
Antonio Mallia <me@antoniomallia.it>
2019-02-18 14:04:11 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,6 +50,13 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
+
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
 source_suffix = ['.rst', '.md']
 # source_suffix = '.rst'
 
