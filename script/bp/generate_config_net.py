e36320882f20208f353841a467550b7c0c2c2971
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2019-03-29 11:30:28 -0400

    Add params for minsize and iterations

--- script/bp/generate_config_net.py
+++ script/bp/generate_config_net.py
@@ -18,19 +18,23 @@ def make_net(left, right, iterations, level, minsize, out):
     print(level + 4, iterations, left, q1, q3, right, 0, file=out)
     print(level + 4, iterations, q1, mid, mid, q3, 0, file=out)
 
-    if q1 - left > minsize:
+    if right - left > minsize:
         make_net(left, mid, iterations, level + 5, minsize, out)
         make_net(mid, right, iterations, level + 5, minsize, out)
 
-def generate(ndoc, out):
-    make_net(0, ndoc, 10, 0, 20, out)
+def generate(ndoc, out, iterations, minsize):
+    make_net(0, ndoc, iterations, 0, minsize, out)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate BP config file.')
+    parser.add_argument('--iterations', type=int, default=10,
+                        help='Number of iterations.')
+    parser.add_argument('--minsize', type=int, default=20,
+                        help='Size limit for recursion.')
     parser.add_argument('ndoc', type=int,
                         help='Number of document in collection.')
     parser.add_argument('-o', '--output',
                         type=argparse.FileType('w'), default=sys.stdout,
                         help='Ouptut config file (if absent, write to stdout)')
     args = parser.parse_args()
-    generate(args.ndoc, args.output)
+    generate(args.ndoc, args.output, args.iterations, args.minsize)
