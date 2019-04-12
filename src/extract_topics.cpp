6242118b30c9dc22dbd34af534d382f663b8c393
Antonio Mallia <me@antoniomallia.it>
2019-04-12 00:21:43 -0400

    Add more

--- src/extract_topics.cpp
+++ src/extract_topics.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <string>
+#include <optional>
 
 #include "CLI/CLI.hpp"
 namespace detail {
@@ -7,13 +8,42 @@ namespace detail {
 static std::string const TOP       = "<top>";
 static std::string const TOP_END   = "</top>";
 static std::string const NUM       = "<num>";
+static std::string const NUM_ATT   = "Number:";
 static std::string const NUM_END   = "</num>";
 static std::string const TITLE     = "<title>";
 static std::string const TITLE_END = "</title>";
 static std::string const DESC      = "<desc>";
+static std::string const DESC_ATT   = "Description:";
 static std::string const DESC_END  = "</desc>";
-static std::string const NAR       = "<narr>";
-static std::string const NAR_END   = "</narr>";
+static std::string const NARR       = "<narr>";
+static std::string const NARR_ATT   = "Narrative:";
+static std::string const NARR_END   = "</narr>";
+
+std::string ltrim(const std::string &&str) {
+    auto trimmed = str;
+    auto it2     = std::find_if(trimmed.begin(), trimmed.end(), [](char ch) {
+        return !std::isspace<char>(ch, std::locale::classic());
+    });
+    trimmed.erase(trimmed.begin(), it2);
+    return trimmed;
+}
+
+std::string rtrim(const std::string &&str) {
+    auto trimmed = str;
+    auto it1     = std::find_if(trimmed.rbegin(), trimmed.rend(), [](char ch) {
+        return !std::isspace<char>(ch, std::locale::classic());
+    });
+    trimmed.erase(it1.base(), trimmed.end());
+    return trimmed;
+}
+std::string trim(const std::string &&str) { return ltrim(rtrim(std::forward<decltype(str)>(str))); }
+
+struct Topic {
+    std::string num;
+    std::string title;
+    std::string desc;
+    std::string narr;
+};
 
 bool consume(std::istream &is, std::string const &token) {
     is >> std::ws;
@@ -41,30 +71,43 @@ std::ostream &read_until(std::istream &is, Pred pred, std::ostream &os) {
     return os;
 }
 
-void next_record(std::ifstream &is) {
+std::optional<Topic> next_topic(std::ifstream &is) {
+    if(is.eof()) return std::nullopt;
+    Topic              topic;
     std::ostringstream os;
     is >> std::ws;
-
-    detail::consume(is, TOP);
+    if(not detail::consume(is, TOP)){
+        return std::nullopt;
+    };
     detail::consume(is, NUM);
+    detail::consume(is, NUM_ATT);
     read_until(is, [](auto ch) { return ch == '<'; }, os);
+    topic.num = trim(os.str());
+    detail::consume(is, NUM_END);
+
+    os.clear();
     detail::consume(is, TITLE);
     read_until(is, [](auto ch) { return ch == '<'; }, os);
-    detail::consume(is, DESC);
-    read_until(is, [](auto ch) { return ch == '<'; }, os);
-    detail::consume(is, NAR);
-    read_until(is, [](auto ch) { return ch == '<'; }, os);
+    topic.title = trim(os.str());
+    detail::consume(is, TITLE_END);
 
-    while (read_until(is, [](auto ch) { return ch == '<'; }, os)) {
-        if (is.peek() == std::istream::traits_type::eof()) {
-            break;
-        }
-        if (consume(is, TOP_END)) {
-            break;
-        }
-        os.put(is.get());
-    }
-    std::cout << os.str() << std::endl;
+    os.clear();
+    detail::consume(is, DESC);
+    detail::consume(is, DESC_ATT);
+    read_until(is, [](auto ch) { return ch == '<'; }, os);
+    topic.desc = trim(os.str());
+    detail::consume(is, DESC_END);
+
+    os.clear();
+    detail::consume(is, NARR);
+    detail::consume(is, NARR_ATT);
+    read_until(is, [](auto ch) { return ch == '<'; }, os);
+    topic.narr = trim(os.str());
+    detail::consume(is, NARR_END);
+
+    detail::consume(is, TOP_END);
+
+    return std::make_optional(topic);
 }
 
 }  // namespace detail
@@ -79,5 +122,9 @@ int main(int argc, char const *argv[]) {
     CLI11_PARSE(app, argc, argv);
 
     std::ifstream infile(input_filename);
-    detail::next_record(infile);
+    std::vector<detail::Topic> topics;
+    while(auto topic = detail::next_topic(infile)){
+        topics.push_back(topic.value());
+    }
+
 }
