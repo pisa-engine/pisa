d50e9e6cc3525821b6c3b1f46c3e38baa4aa0102
Antonio Mallia <me@antoniomallia.it>
2019-04-14 17:11:18 +0200

    Added tests

--- src/extract_topics.cpp
+++ src/extract_topics.cpp
@@ -1,100 +1,9 @@
 #include <fstream>
-#include <optional>
 #include <string>
 
+#include "pisa/query/trec_topic_reader.hpp"
+
 #include "CLI/CLI.hpp"
-
-namespace detail {
-
-static std::string const TOP       = "<top>";
-static std::string const TOP_END   = "</top>";
-static std::string const NUM       = "<num>";
-static std::string const NUM_ATT   = "Number:";
-static std::string const NUM_END   = "</num>";
-static std::string const TITLE     = "<title>";
-static std::string const TITLE_END = "</title>";
-static std::string const DESC      = "<desc>";
-static std::string const DESC_ATT  = "Description:";
-static std::string const DESC_END  = "</desc>";
-static std::string const NARR      = "<narr>";
-static std::string const NARR_ATT  = "Narrative:";
-static std::string const NARR_END  = "</narr>";
-
-struct Topic {
-    std::string num;
-    std::string title;
-    std::string desc;
-    std::string narr;
-};
-
-void consume(std::istream &is, std::string const &token, bool strict = true) {
-    is >> std::ws;
-    for (auto pos = token.begin(); pos != token.end(); ++pos) {
-        if (is.get() != *pos) {
-            is.unget();
-            for (auto rpos = std::reverse_iterator(pos); rpos != token.rend(); ++rpos) {
-                is.putback(*rpos);
-            }
-            if (strict) {
-                throw std::runtime_error("Could not consume tag: " + token);
-            }
-            break;
-        }
-    }
-}
-
-template <typename Pred>
-std::ostream &read_until(std::istream &is, Pred pred, std::ostream &os) {
-    is >> std::ws;
-    while (not is.eof()) {
-        if (is.peek() == std::istream::traits_type::eof() or pred(is.peek())) {
-            break;
-        }
-        os.put(is.get());
-    }
-    return os;
-}
-
-std::optional<Topic> next_topic(std::ifstream &is) {
-    is >> std::ws;
-    if (is.eof()) return std::nullopt;
-
-    Topic              topic;
-    std::ostringstream os;
-
-    detail::consume(is, TOP);
-
-    detail::consume(is, NUM);
-    detail::consume(is, NUM_ATT);
-    read_until(is, [](auto ch) { return ch == '<'; }, os);
-    topic.num = boost::algorithm::trim(os.str());
-    detail::consume(is, NUM_END, false);
-
-    os.str("");
-    detail::consume(is, TITLE);
-    read_until(is, [](auto ch) { return ch == '<'; }, os);
-    topic.title = boost::algorithm::trim(os.str());
-    detail::consume(is, TITLE_END, false);
-
-    os.str("");
-    detail::consume(is, DESC);
-    detail::consume(is, DESC_ATT, false);
-    read_until(is, [](auto ch) { return ch == '<'; }, os);
-    topic.desc = boost::algorithm::trim(os.str());
-    detail::consume(is, DESC_END, false);
-
-    os.str("");
-    detail::consume(is, NARR);
-    detail::consume(is, NARR_ATT, false);
-    read_until(is, [](auto ch) { return ch == '<'; }, os);
-    topic.narr = boost::algorithm::trim(os.str());
-    detail::consume(is, NARR_END, false);
-
-    detail::consume(is, TOP_END);
-    return std::make_optional(topic);
-}
-
-}  // namespace detail
 
 int main(int argc, char const *argv[]) {
     std::string input_filename;
@@ -105,7 +14,6 @@ int main(int argc, char const *argv[]) {
     app.add_option("-o,--output", output_basename, "Output basename")->required();
     CLI11_PARSE(app, argc, argv);
 
-    std::ifstream infile(input_filename);
     std::ofstream title_file;
     title_file.open(output_basename + ".title");
     std::ofstream desc_file;
@@ -113,7 +21,10 @@ int main(int argc, char const *argv[]) {
     std::ofstream narr_file;
     narr_file.open(output_basename + ".narr");
 
-    while (auto topic = detail::next_topic(infile)) {
+    auto                    input_file = std::ifstream(input_filename);
+    pisa::trec_topic_reader reader(input_file);
+
+    while (auto topic = reader.next_topic()) {
         auto t = *topic;
         title_file << t.num << ":" << t.title << std::endl;
         desc_file << t.num << ":" << t.desc << std::endl;
---
+++ test/test_trec_topic_reader.cpp
@@ -0,0 +1,140 @@
+#define CATCH_CONFIG_MAIN
+#include <optional>
+#include "catch2/catch.hpp"
+
+#include "pisa/query/trec_topic_reader.hpp"
+
+using namespace pisa;
+
+TEST_CASE("Read topic", "[unit]") {
+    std::istringstream is(
+        "<top>\n"
+        "<num> Number: 301 \n"
+        "<title> title here. \n"
+        "<desc> Description: \n"
+        "Some description here. \n"
+        "<narr> Narrative:\n"
+        "Some narrative content. \n"
+        "</top>\n");
+
+    pisa::trec_topic_reader reader(is);
+    auto                    topic = reader.next_topic();
+    REQUIRE(topic);
+    REQUIRE((*topic).num == "301");
+    REQUIRE((*topic).title == "title here.");
+    REQUIRE((*topic).desc == "Some description here.");
+    REQUIRE((*topic).narr == "Some narrative content.");
+    REQUIRE(reader.next_topic() == std::nullopt);
+}
+
+TEST_CASE("Read multiple topics", "[unit]") {
+    std::istringstream is(
+        "<top>\n"
+        "<num> Number: 301 \n"
+        "<title> title here. \n"
+        "<desc> Description: \n"
+        "Some description here. \n"
+        "<narr> Narrative:\n"
+        "Some narrative content. \n"
+        "</top>\n"
+        "\n\n\n\n"
+        "<top>\n"
+        "<num> Number: 302 \n"
+        "<title> other title. \n"
+        "<desc>  \n"
+        "Some other description. \n"
+        "<narr>\n"
+        "Some other narrative. \n"
+        "</top>\n");
+
+    pisa::trec_topic_reader reader(is);
+    auto                    topic = reader.next_topic();
+    REQUIRE(topic);
+    REQUIRE((*topic).num == "301");
+    REQUIRE((*topic).title == "title here.");
+    REQUIRE((*topic).desc == "Some description here.");
+    REQUIRE((*topic).narr == "Some narrative content.");
+    topic = reader.next_topic();
+    REQUIRE(topic);
+    REQUIRE((*topic).num == "302");
+    REQUIRE((*topic).title == "other title.");
+    REQUIRE((*topic).desc == "Some other description.");
+    REQUIRE((*topic).narr == "Some other narrative.");
+    REQUIRE(reader.next_topic() == std::nullopt);
+}
+
+TEST_CASE("Read topic with closing tags", "[unit]") {
+    std::istringstream is(
+        "<top>\n"
+        "<num> Number: 301 \n"
+        "<title> title here. \n"
+        "</title>"
+        "<desc>  \n"
+        "Some description here. </desc>"
+        "<narr> Narrative:\n"
+        "Some narrative content. \n"
+        "</narr>"
+        "</top>\n");
+
+    pisa::trec_topic_reader reader(is);
+    auto                    topic = reader.next_topic();
+    REQUIRE(topic);
+    REQUIRE((*topic).num == "301");
+    REQUIRE((*topic).title == "title here.");
+    REQUIRE((*topic).desc == "Some description here.");
+    REQUIRE((*topic).narr == "Some narrative content.");
+    REQUIRE(reader.next_topic() == std::nullopt);
+}
+
+TEST_CASE("Invalid topic", "[unit]") {
+    {
+        std::istringstream is(
+            "<top>\n"
+            "Number: 301 \n"
+            "</top>\n");
+
+        pisa::trec_topic_reader reader(is);
+        REQUIRE_THROWS(reader.next_topic());
+    }
+    {
+        std::istringstream is(
+            "<top>\n"
+            "<num>Number: 301 \n"
+            "</top>\n");
+
+        pisa::trec_topic_reader reader(is);
+        REQUIRE_THROWS(reader.next_topic());
+    }
+    {
+        std::istringstream is(
+            "<top>\n"
+            "<num>Number: 301 \n"
+            "<title> title here. \n"
+            "</top>\n");
+
+        pisa::trec_topic_reader reader(is);
+        REQUIRE_THROWS(reader.next_topic());
+    }
+    {
+        std::istringstream is(
+            "<top>\n"
+            "<num>Number: 301 \n"
+            "<title> title here. \n"
+            "<desc> description here. \n"
+            "</top>\n");
+
+        pisa::trec_topic_reader reader(is);
+        REQUIRE_THROWS(reader.next_topic());
+    }
+    {
+        std::istringstream is(
+            "<top>\n"
+            "<num>Number: 301 \n"
+            "<title> title here. \n"
+            "<desc> description here. \n"
+            "<narr> narrative here. \n");
+
+        pisa::trec_topic_reader reader(is);
+        REQUIRE_THROWS(reader.next_topic());
+    }
+}
