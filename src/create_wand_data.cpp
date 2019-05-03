3f2257a6cfe789ca6a9b7519e9a21bbf5875f351
JMMackenzie <joel.m.mackenzie@gmail.com>
2019-05-03 12:37:15 +1000

    Docs and flag change

--- src/create_wand_data.cpp
+++ src/create_wand_data.cpp
@@ -31,7 +31,7 @@ int main(int argc, const char **argv)
     app.add_option("-c,--collection", input_basename, "Collection basename")->required();
     app.add_option("-o,--output", output_filename, "Output filename")->required();
     auto var_block_opt = app.add_flag("--variable-block", variable_block, "Variable length blocks");
-    auto var_block_param_opt = app.add_option("-b,--block", fixed_block_size, "Block size for fixed-length blocks")->excludes(var_block_opt);
+    auto var_block_param_opt = app.add_option("-b,--block-size", fixed_block_size, "Block size for fixed-length blocks")->excludes(var_block_opt);
     app.add_option("-l,--lambda", lambda, "Lambda parameter for variable blocks")->excludes(var_block_param_opt);
     app.add_flag("--compress", compress, "Compress additional data");
     app.add_flag("--range", range, "Create docid-range based data")->excludes(var_block_opt);
