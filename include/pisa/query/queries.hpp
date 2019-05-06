d56fb8c640f97a00823dcbdc65febece1a2afa67
JMMackenzie <joel.m.mackenzie@gmail.com>
2019-05-06 17:06:31 +1000

    Add Antionio's Ranked AND test

--- include/pisa/query/queries.hpp
+++ include/pisa/query/queries.hpp
@@ -158,6 +158,7 @@ namespace query {
 
 #include "algorithm/and_query.hpp"
 #include "algorithm/block_max_maxscore_query.hpp"
+#include "algorithm/block_max_ranked_and_query.hpp"
 #include "algorithm/block_max_wand_query.hpp"
 #include "algorithm/maxscore_query.hpp"
 #include "algorithm/or_query.hpp"
