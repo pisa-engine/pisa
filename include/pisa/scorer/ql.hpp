ca8d742e52e7370ee4cf861008e58206aff4b9e2
Antonio <me@antoniomallia.it>
2019-06-25 17:29:02 -0400

    Fix

--- include/pisa/scorer/ql.hpp
+++ include/pisa/scorer/ql.hpp
@@ -13,7 +13,7 @@ namespace pisa {
         double term_id;
 
         [[nodiscard]] float operator()(uint32_t doc, uint32_t freq) const {
-            float numerator = freq + mu + wdata.collection_term_frequency(term_id) / wdata.collection_len();
+            float numerator = freq + mu * wdata.collection_term_frequency(term_id) / wdata.collection_len();
             float denominator = document_len + mu;
             return std::log(numerator/denominator);
         }
