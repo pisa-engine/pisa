d1d06cb4ceefa079807eca995f3497c1504a982c
Antonio <me@antoniomallia.it>
2019-06-25 17:31:52 -0400

    Clang format

--- include/pisa/scorer/ql.hpp
+++ include/pisa/scorer/ql.hpp
@@ -1,22 +1,23 @@
 #pragma once
 
-#include <cstdint>
-#include <cmath>
 #include <algorithm>
+#include <cmath>
+#include <cstdint>
 namespace pisa {
 
-    template <typename Wand>
-    struct ql {
-        static constexpr float mu = 2000;
-        const Wand& wdata;
-        double document_len;
-        double term_id;
+template <typename Wand>
+struct ql {
+    static constexpr float mu = 2000;
+    const Wand&            wdata;
+    double                 document_len;
+    double                 term_id;
 
-        [[nodiscard]] float operator()(uint32_t doc, uint32_t freq) const {
-            float numerator = freq + mu * wdata.collection_term_frequency(term_id) / wdata.collection_len();
-            float denominator = document_len + mu;
-            return std::log(numerator/denominator);
-        }
-    };
+    [[nodiscard]] float operator()(uint32_t doc, uint32_t freq) const {
+        float numerator =
+            freq + mu * wdata.collection_term_frequency(term_id) / wdata.collection_len();
+        float denominator = document_len + mu;
+        return std::log(numerator / denominator);
+    }
+};
 
-}
+}  // namespace pisa
