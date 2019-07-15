83f3b6a2967d53d92f9058f4301bb448c6eb1de8
Antonio Mallia <me@antoniomallia.it>
2019-07-15 13:02:22 +0200

    Reset wand_data.hpp

--- include/pisa/wand_data.hpp
+++ include/pisa/wand_data.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <numeric>
+
 #include "boost/variant.hpp"
 #include "spdlog/spdlog.h"
 
@@ -21,17 +21,19 @@ class wand_data {
     wand_data() {}
 
     template <typename LengthsIterator>
-    wand_data(LengthsIterator len_it, uint64_t num_docs, binary_freq_collection const &coll,
-              BlockSize block_size) {
-        std::vector<float>    doc_lens(num_docs);
-        std::vector<float>    max_term_weight;
-        std::vector<uint32_t> collection_terms_count;
-        global_parameters     params;
-        double                lens_sum = 0;
+    wand_data(LengthsIterator len_it,
+              uint64_t num_docs,
+              binary_freq_collection const &coll,
+              BlockSize block_size)
+    {
+        std::vector<uint32_t> doc_lens(num_docs);
+        std::vector<float> max_term_weight;
+        global_parameters params;
+        double lens_sum = 0;
         spdlog::info("Reading sizes...");
 
         for (size_t i = 0; i < num_docs; ++i) {
-            float len   = *len_it++;
+            float len = *len_it++;
             doc_lens[i] = len;
             lens_sum += len;
         }
@@ -42,8 +44,6 @@ class wand_data {
         {
             pisa::progress progress("Processing posting lists", coll.size());
             for (auto const &seq : coll) {
-                uint32_t term_count = std::accumulate(seq.freqs.begin(), seq.freqs.end(), 0);
-                collection_terms_count.push_back(term_count);
                 auto v = builder.add_sequence(seq, coll, doc_lens, avg_len, block_size);
                 max_term_weight.push_back(v);
                 progress.update(1);
@@ -51,19 +51,18 @@ class wand_data {
         }
         builder.build(m_block_wand);
         m_doc_lens.steal(doc_lens);
-        m_collection_terms_count.steal(collection_terms_count);
         m_max_term_weight.steal(max_term_weight);
-        m_lens_sum = lens_sum;
-        m_avg_len  = avg_len;
+        m_avg_len = avg_len;
     }
 
-    double collection_len() const { return m_lens_sum; }
-
-    float doc_len(uint64_t doc_id) const { return m_doc_lens[doc_id]; }
-
-    uint32_t collection_term_frequency(uint64_t term_id) const {
-        return m_collection_terms_count[term_id];
+    inline float norm_len(uint64_t doc_id) const
+    {
+        return m_doc_lens[doc_id] / m_avg_len;
     }
+
+    size_t doc_len(uint64_t doc_id) const { return m_doc_lens[doc_id]; }
+
+    float avg_len() const { return m_avg_len; }
 
     float max_term_weight(uint64_t list) const { return m_max_term_weight[list]; }
 
@@ -72,18 +71,16 @@ class wand_data {
     const block_wand_type &get_block_wand() const { return m_block_wand; }
 
     template <typename Visitor>
-    void map(Visitor &visit) {
-        visit(m_block_wand, "m_block_wand")(m_collection_terms_count, "m_collection_terms_count")(
-            m_doc_lens, "m_doc_lens")(m_max_term_weight, "m_max_term_weight")(
-            m_lens_sum, "m_lens_sum")(m_avg_len, "m_avg_len");
+    void map(Visitor &visit)
+    {
+        visit(m_block_wand, "m_block_wand")(m_doc_lens, "m_doc_lens")(m_avg_len, "m_avg_len")(
+            m_max_term_weight, "m_max_term_weight");
     }
 
    private:
-    block_wand_type                   m_block_wand;
-    mapper::mappable_vector<uint32_t> m_collection_terms_count;
-    mapper::mappable_vector<float>    m_doc_lens;
-    mapper::mappable_vector<float>    m_max_term_weight;
-    double                            m_lens_sum;
-    float avg_len m_avg_len;
+    block_wand_type m_block_wand;
+    mapper::mappable_vector<uint32_t> m_doc_lens;
+    float m_avg_len;
+    mapper::mappable_vector<float> m_max_term_weight;
 };
-}  // namespace pisa
+} // namespace pisa
