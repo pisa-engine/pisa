31543fc52e8fb7a04e3ea0cc8b558f814b14b6b2
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2019-08-23 11:06:09 +1000

    remove binary path; use up to date option

--- examples/index_robust04.sh
+++ examples/index_robust04.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-PISA_PREFIX=~/src/pisa
-PISA_BIN=$PISA_PREFIX/build/bin
-THREADS=20
+set -e
+
+THREADS=$(nproc)
 
 COLLECTION=(/research/remote/collections/TREC/disk4/fr.gz
 	    /research/remote/collections/TREC/disk4/ft.gz
@@ -12,19 +12,16 @@ COLLECTION=(/research/remote/collections/TREC/disk4/fr.gz
 ROB_PREFIX=./robust04
 
 # Generate robust04 robust04.terms robust04.urls robust04.documents
-zcat ${COLLECTION[@]} | $PISA_BIN/parse_collection -j $THREADS -f trectext --content-parser html --stemmer krovetz -o $ROB_PREFIX
+zcat ${COLLECTION[@]} | parse_collection -j $THREADS -f trectext --content-parser html --stemmer krovetz -o $ROB_PREFIX
 
 # Generate robust04.docs robust04.freqs robust04.sizes
-$PISA_BIN/invert -i $ROB_PREFIX -o $ROB_PREFIX -j 40 --term-count $(wc -l < $ROB_PREFIX.terms)
+invert -i $ROB_PREFIX -o $ROB_PREFIX -j $THREADS --term-count $(wc -l < $ROB_PREFIX.terms)
 
 # Generate robust04.index.opt
-$PISA_BIN/create_freq_index -t opt -c $ROB_PREFIX -o $ROB_PREFIX.index.opt --check
+create_freq_index -t pefopt -c $ROB_PREFIX -o $ROB_PREFIX.index.opt --check
 
 # Generate robust04.wand
-$PISA_BIN/create_wand_data -c $ROB_PREFIX -o $ROB_PREFIX.wand
+create_wand_data -c $ROB_PREFIX -o $ROB_PREFIX.wand
 
-# Test queries
-$PISA_BIN/queries -t opt -a block_max_wand -i $ROB_PREFIX.index.opt -w $ROB_PREFIX.wand -q $PISA_PREFIX/test/test_data/queries
-
-# Test indri output
-echo '301:international organized crime' | $PISA_BIN/evaluate_queries -t opt -a block_max_wand -i $ROB_PREFIX.index.opt -w $ROB_PREFIX.wand --documents $ROB_PREFIX.doclex --terms $ROB_PREFIX.termlex
+# Test TREC output
+echo '301:international organized crime' | evaluate_queries -t pefopt -a block_max_wand -i $ROB_PREFIX.index.opt -w $ROB_PREFIX.wand --documents $ROB_PREFIX.doclex --terms $ROB_PREFIX.termlex
