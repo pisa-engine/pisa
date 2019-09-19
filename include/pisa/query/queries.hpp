09e354c13ca2326f8b6121e9fc64e2e0f906777b
Agustín González <agustin.ang.92@gmail.com>
2019-09-19 20:09:57 -0300

    Update query processing (#244)
    Fix ID tokenization problems
    
    - Refactor queries.hpp to avoid tokenization when using query ids
    - Make the 'stopwords' parameter require 'terms' option
    - Fix term existence checking when stopwords are load in term processor

--- include/pisa/query/queries.hpp
+++ include/pisa/query/queries.hpp
@@ -1,26 +1,20 @@
 #pragma once
 
-#include <functional>
 #include <iostream>
 #include <memory>
 #include <optional>
 #include <sstream>
 #include <string>
 #include <string_view>
-#include <unordered_map>
 
-#include <KrovetzStemmer/KrovetzStemmer.hpp>
-#include <Porter2/Porter2.hpp>
 #include <boost/algorithm/string.hpp>
-#include <mio/mmap.hpp>
 #include <range/v3/view/enumerate.hpp>
 #include <spdlog/spdlog.h>
 
 #include "index_types.hpp"
-#include "io.hpp"
-#include "payload_vector.hpp"
 #include "query/queries.hpp"
 #include "scorer/score_function.hpp"
+#include "term_processor.hpp"
 #include "tokenizer.hpp"
 #include "topk_queue.hpp"
 #include "util/util.hpp"
@@ -32,70 +26,108 @@ namespace pisa {
 
 using term_id_type = uint32_t;
 using term_id_vec = std::vector<term_id_type>;
-using TermProcessor = std::function<std::optional<term_id_type>(std::string &&)>;
 
 struct Query {
     std::optional<std::string> id;
-    std::vector<term_id_type>  terms;
-    std::vector<float>  term_weights;
+    std::vector<term_id_type> terms;
+    std::vector<float> term_weights;
 };
 
-[[nodiscard]] inline auto parse_query(
-    std::string const &query_string,
-    TermProcessor process_term,
-    std::optional<std::unordered_set<term_id_type>> const &stopwords = std::nullopt) -> Query
+[[nodiscard]] auto split_query_at_colon(std::string const &query_string)
+    -> std::pair<std::optional<std::string>, std::string_view>
 {
-    std::optional<std::string> id = std::nullopt;
-    std::vector<term_id_type> parsed_query;
+    // query id : terms (or ids)
     auto colon = std::find(query_string.begin(), query_string.end(), ':');
+    std::optional<std::string> id;
     if (colon != query_string.end()) {
         id = std::string(query_string.begin(), colon);
     }
     auto pos = colon == query_string.end() ? query_string.begin() : std::next(colon);
     auto raw_query = std::string_view(&*pos, std::distance(pos, query_string.end()));
-    TermTokenizer tokenizer(raw_query);
-    for (auto term_iter = tokenizer.begin(); term_iter != tokenizer.end(); ++term_iter) {
-        auto raw_term = *term_iter;
-        try {
-            auto processed = process_term(std::string(raw_term));
-            if (processed) {
-                if (not stopwords or stopwords->find(*processed) == stopwords->end()) {
-                    parsed_query.push_back(std::move(*processed));
-                } else {
-                    spdlog::warn("Term `{}` is a stop word and will be ignored", *processed);
-                }
-            } else {
-                spdlog::warn("Term `{}` not found and will be ignored", raw_term);
-            }
-        } catch (std::invalid_argument& err) {
-            spdlog::warn("Could not parse `{}` to a number", raw_term);
-        }
-    }
-    return {id, parsed_query, {}};
+    return {std::move(id), std::move(raw_query)};
 }
 
-bool read_query(term_id_vec &ret, std::istream &is = std::cin,
-                std::function<term_id_type(std::string)> process_term = [](auto str) {
-                    return std::stoi(str);
-                }) {
+[[nodiscard]] auto parse_query_terms(std::string const &query_string, TermProcessor term_processor)
+    -> Query
+{
+    auto [id, raw_query] = split_query_at_colon(query_string);
+    TermTokenizer tokenizer(raw_query);
+    std::vector<term_id_type> parsed_query;
+    for (auto term_iter = tokenizer.begin(); term_iter != tokenizer.end(); ++term_iter) {
+        auto raw_term = *term_iter;
+        auto term = term_processor(raw_term);
+        if (term) {
+            if (!term_processor.is_stopword(*term)) {
+                parsed_query.push_back(std::move(*term));
+            } else {
+                spdlog::warn("Term `{}` is a stopword and will be ignored", *term);
+            }
+        } else {
+            spdlog::warn("Term `{}` not found and will be ignored", raw_term);
+        }
+    }
+    return {std::move(id), std::move(parsed_query), {}};
+}
+
+[[nodiscard]] auto parse_query_ids(std::string const &query_string) -> Query
+{
+    auto [id, raw_query] = split_query_at_colon(query_string);
+    std::vector<term_id_type> parsed_query;
+    std::vector<std::string> term_ids;
+    boost::split(term_ids, raw_query, boost::is_any_of("\t, ,\v,\f,\r,\n"));
+
+    auto is_empty = [](const std::string &val) { return val.empty(); };
+    // remove_if move matching elements to the end, preparing them for erase.
+    term_ids.erase(std::remove_if(term_ids.begin(), term_ids.end(), is_empty), term_ids.end());
+
+    try {
+        auto to_int = [](const std::string &val) { return std::stoi(val); };
+        std::transform(term_ids.begin(), term_ids.end(), std::back_inserter(parsed_query), to_int);
+    } catch (std::invalid_argument &err) {
+        spdlog::error("Could not parse term identifiers of query `{}`", raw_query);
+        exit(1);
+    }
+    return {std::move(id), std::move(parsed_query), {}};
+}
+
+[[nodiscard]] std::function<void(const std::string)> compute_parse_query_function(
+    std::vector<Query> &queries,
+    const std::optional<std::string> terms_file,
+    const std::optional<std::string> stopwords_filename,
+    const std::optional<std::string> stemmer_type) {
+    if (terms_file) {
+        auto term_processor = TermProcessor(terms_file, stopwords_filename, stemmer_type);
+        return [&](std::string const &query_line) {
+            queries.push_back(parse_query_terms(query_line, term_processor));
+        };
+    } else {
+        return
+            [&](std::string const &query_line) { queries.push_back(parse_query_ids(query_line)); };
+    }
+}
+
+bool read_query(term_id_vec &ret, std::istream &is = std::cin)
+{
     ret.clear();
     std::string line;
     if (!std::getline(is, line)) {
         return false;
     }
-    ret = parse_query(line, process_term).terms;
+    ret = parse_query_ids(line).terms;
     return true;
 }
 
-void remove_duplicate_terms(term_id_vec &terms) {
+void remove_duplicate_terms(term_id_vec &terms)
+{
     std::sort(terms.begin(), terms.end());
     terms.erase(std::unique(terms.begin(), terms.end()), terms.end());
 }
 
 typedef std::pair<uint64_t, uint64_t> term_freq_pair;
-typedef std::vector<term_freq_pair>   term_freq_vec;
+typedef std::vector<term_freq_pair> term_freq_vec;
 
-term_freq_vec query_freqs(term_id_vec terms) {
+term_freq_vec query_freqs(term_id_vec terms)
+{
     term_freq_vec query_term_freqs;
     std::sort(terms.begin(), terms.end());
     // count query term frequencies
@@ -106,55 +138,10 @@ term_freq_vec query_freqs(term_id_vec terms) {
             query_term_freqs.back().second += 1;
         }
     }
-
     return query_term_freqs;
 }
 
-namespace query {
-
-    TermProcessor term_processor(std::optional<std::string> terms_file,
-                                 std::optional<std::string> stemmer_type)
-    {
-        if (terms_file) {
-            auto source = std::make_shared<mio::mmap_source>(terms_file->c_str());
-            auto terms = Payload_Vector<>::from(*source);
-            auto to_id = [source = std::move(source),
-                          terms = std::move(terms)](auto str) -> std::optional<term_id_type>
-            {
-                auto pos = std::lower_bound(terms.begin(), terms.end(), std::string_view(str));
-                if (*pos == std::string_view(str)) {
-                    return std::distance(terms.begin(), pos);
-                }
-                return std::nullopt;
-            };
-            if (not stemmer_type) {
-                return [=](auto str) {
-                    boost::algorithm::to_lower(str);
-                    return to_id(str);
-                };
-            }
-            if (*stemmer_type == "porter2") {
-                return [=](auto str) {
-                    boost::algorithm::to_lower(str);
-                    stem::Porter2 stemmer{};
-                    return to_id(stemmer.stem(str));
-                };
-            }
-            if (*stemmer_type == "krovetz") {
-                return [=](auto str) {
-                    boost::algorithm::to_lower(str);
-                    stem::KrovetzStemmer stemmer{};
-                    return to_id(stemmer.kstem_stemmer(str));
-                };
-            }
-            throw std::invalid_argument("Unknown stemmer");
-        } else {
-            return [](auto str) { return std::make_optional<term_id_type>(std::stoi(str)); };
-        }
-    }
-
-}  // namespace query
-}  // namespace pisa
+} // namespace pisa
 
 #include "algorithm/and_query.hpp"
 #include "algorithm/block_max_maxscore_query.hpp"
---
+++ include/pisa/query/term_processor.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <functional>
+#include <optional>
+#include <unordered_set>
+
+#include <KrovetzStemmer/KrovetzStemmer.hpp>
+#include <Porter2/Porter2.hpp>
+#include <boost/algorithm/string.hpp>
+#include <mio/mmap.hpp>
+
+#include "io.hpp"
+#include "payload_vector.hpp"
+
+namespace pisa {
+
+using term_id_type = uint32_t;
+
+class TermProcessor {
+   private:
+    std::unordered_set<term_id_type> stopwords;
+
+    // Method implemented in constructor according to the specified stemmer.
+    std::function<std::optional<term_id_type>(std::string)> _to_id;
+
+   public:
+    TermProcessor(const std::optional<std::string> terms_file,
+                  const std::optional<std::string> stopwords_filename,
+                  const std::optional<std::string> stemmer_type)
+    {
+        auto source = std::make_shared<mio::mmap_source>(terms_file->c_str());
+        auto terms = Payload_Vector<>::from(*source);
+        auto to_id = [source = std::move(source),
+                      terms = std::move(terms)](auto str) -> std::optional<term_id_type> {
+            // Note: the lexicographical order of the terms matters.
+            auto pos = std::lower_bound(terms.begin(), terms.end(), std::string_view(str));
+            if (*pos == std::string_view(str)) {
+                return std::distance(terms.begin(), pos);
+            }
+            return std::nullopt;
+        };
+
+        // Implements '_to_id' method.
+        if (not stemmer_type) {
+            _to_id = [=](auto str) {
+                boost::algorithm::to_lower(str);
+                return to_id(std::move(str));
+            };
+        } else if (*stemmer_type == "porter2") {
+            _to_id = [=](auto str) {
+                boost::algorithm::to_lower(str);
+                stem::Porter2 stemmer{};
+                return to_id(std::move(stemmer.stem(str)));
+            };
+        } else if (*stemmer_type == "krovetz") {
+            _to_id = [=](auto str) {
+                boost::algorithm::to_lower(str);
+                stem::KrovetzStemmer stemmer{};
+                stemmer.kstem_stemmer(str);
+                return to_id(stemmer.kstem_stemmer(std::move(str)));
+            };
+        } else {
+            throw std::invalid_argument("Unknown stemmer");
+        }
+
+        // Loads stopwords.
+        if (stopwords_filename) {
+            std::ifstream is(*stopwords_filename);
+            io::for_each_line(is, [&](auto &&word) {
+                if (auto processed_term = _to_id(std::move(word)); processed_term.has_value()) {
+                    stopwords.insert(*processed_term);
+                }
+            });
+        }
+    }
+
+    std::optional<term_id_type> operator()(std::string token) { return _to_id(token); }
+
+    bool is_stopword(const term_id_type term) { return stopwords.find(term) != stopwords.end(); }
+
+    std::vector<term_id_type> get_stopwords()
+    {
+        std::vector<term_id_type> v;
+        v.insert(v.end(), stopwords.begin(), stopwords.end());
+        sort(v.begin(), v.end());
+        return v;
+    }
+};
+} // namespace pisa
