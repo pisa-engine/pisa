1e2bd49a4941da176bedb924fd581e58fde99623
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2019-09-30 19:25:47 -0400

    Intersecting combination of terms in query

---
+++ include/pisa/intersection.hpp
@@ -0,0 +1,97 @@
+#include <bitset>
+#include <cstddef>
+#include <optional>
+#include <vector>
+
+#include "query/algorithm/and_query.hpp"
+#include "query/queries.hpp"
+
+namespace pisa {
+
+namespace intersection {
+
+    /// Mask is set to a static-sized bitset for performance.
+    /// Using dynamic bitset slows down a lot, and these operations will be performed on
+    /// much shorter queries than the limit allows for. It makes very little sense to do
+    /// that on anything longer.
+    constexpr std::size_t MAX_QUERY_LEN_EXP = 8;
+    constexpr std::size_t MAX_QUERY_LEN = 1 << MAX_QUERY_LEN_EXP;
+    using Mask = std::bitset<MAX_QUERY_LEN_EXP>;
+
+    /// Returns a filtered copy of `query` containing only terms indicated by ones in the bit mask.
+    [[nodiscard]] inline auto filter(Query const &query, Mask mask) -> Query
+    {
+        if (query.terms.size() > MAX_QUERY_LEN) {
+            throw std::invalid_argument("Queries can be at most 64 terms long");
+        }
+        std::vector<std::uint32_t> terms;
+        std::vector<float> weights;
+        for (std::size_t bitpos = 0; bitpos < query.terms.size(); ++bitpos) {
+            if (((1u << bitpos) & mask.to_ulong()) > 0) {
+                terms.push_back(query.terms.at(bitpos));
+                if (bitpos < query.term_weights.size()) {
+                    weights.push_back(query.term_weights[bitpos]);
+                }
+            }
+        }
+        return Query{query.id, terms, weights};
+    }
+} // namespace intersection
+
+/// Represents information about an intersection of one or more terms of a query.
+struct Intersection {
+    /// Number of postings in the intersection.
+    std::size_t length;
+    /// Maximum partial score in the intersection.
+    float max_score;
+
+    template <typename Index, typename Wand>
+    inline static auto compute(Index const &index,
+                               Wand const &wand,
+                               Query const &query,
+                               std::optional<intersection::Mask> term_mask = std::nullopt)
+        -> Intersection;
+};
+
+template <typename Index, typename Wand>
+inline auto Intersection::compute(Index const &index,
+                                  Wand const &wand,
+                                  Query const &query,
+                                  std::optional<intersection::Mask> term_mask) -> Intersection
+{
+    auto filtered_query = term_mask ? intersection::filter(query, *term_mask) : query;
+    if (filtered_query.terms.size() == 1) {
+        return Intersection{index[filtered_query.terms[0]].size(),
+                            wand.max_term_weight(filtered_query.terms[0])};
+    } else {
+        and_query<true> retrieve{};
+        auto results = retrieve(make_scored_cursors(index, wand, filtered_query), index.num_docs());
+        auto max_element = [&](auto const &vec) -> float {
+            auto order = [](auto const &lhs, auto const &rhs) { return lhs.second < rhs.second; };
+            if (auto pos = std::max_element(results.begin(), results.end(), order);
+                pos != results.end()) {
+                return pos->second;
+            } else {
+                return 0.0;
+            }
+        };
+        float max_score = max_element(results);
+        return Intersection{results.size(), max_score};
+    }
+}
+
+/// Do `func` for all intersections in a query that have a given maximum number of terms.
+/// `Fn` takes `Query` and `Mask`.
+template <typename Fn>
+auto for_all_subsets(Query const &query, std::uint8_t max_term_count, Fn func)
+{
+    auto subset_count = 1u << query.terms.size();
+    for (auto subset = 1u; subset < subset_count; ++subset) {
+        auto mask = intersection::Mask(subset);
+        if (mask.count() <= max_term_count) {
+            func(query, mask);
+        }
+    }
+}
+
+} // namespace pisa
--- src/compute_intersection.cpp
+++ src/compute_intersection.cpp
@@ -1,23 +1,26 @@
 #include <optional>
-#include <vector>
 #include <string>
+#include <vector>
 
 #include <fmt/format.h>
 
 #include "CLI/CLI.hpp"
 #include "index_types.hpp"
+#include "intersection.hpp"
 #include "pisa/cursor/scored_cursor.hpp"
 #include "pisa/query/queries.hpp"
 #include "wand_data_compressed.hpp"
 #include "wand_data_raw.hpp"
 
 using namespace pisa;
+using pisa::intersection::Mask;
 
 template <typename IndexType, typename WandType>
 void intersect(std::string const &index_filename,
                std::optional<std::string> const &wand_data_filename,
                std::vector<Query> const &queries,
-               std::string const &type)
+               std::string const &type,
+               std::optional<std::uint8_t> combinations = std::nullopt)
 {
     IndexType index;
     mio::mmap_source m(index_filename.c_str());
@@ -36,22 +39,26 @@ void intersect(std::string const &index_filename,
         mapper::map(wdata, md, mapper::map_flags::warmup);
     }
 
-    and_query<true> and_q;
-    std::size_t qid = 0;
+    std::size_t qid = 0u;
+
+    auto print_intersection = [&](auto const &query, auto const &mask) {
+        auto intersection = Intersection::compute(index, wdata, query, mask);
+        std::cout << fmt::format("{}\t{}\t{}\t{}\n",
+                                 query.id ? *query.id : std::to_string(qid),
+                                 mask.to_ulong(),
+                                 intersection.length,
+                                 intersection.max_score);
+    };
+
     for (auto const &query : queries) {
-        auto query_id = query.id ? *query.id : std::to_string(qid);
-        if (query.terms.size() == 1) {
-            std::cout << fmt::format("{}\t{}\t{}\n",
-                                     query_id,
-                                     index[query.terms[0]].size(),
-                                     wdata.max_term_weight(query.terms[0]));
+        if (combinations) {
+            for_all_subsets(query, *combinations, print_intersection);
         } else {
-            auto results = and_q(make_scored_cursors(index, wdata, query), index.num_docs());
-            std::sort(results.begin(), results.end(), [](auto const &lhs, auto const &rhs) {
-                return lhs.second > rhs.second;
-            });
-            auto max_score = results.empty() ? 0.0 : results[0].second;
-            std::cout << fmt::format("{}\t{}\t{}\n", query_id, results.size(), max_score);
+            auto intersection = Intersection::compute(index, wdata, query);
+            std::cout << fmt::format("{}\t{}\t{}\n",
+                                     query.id ? *query.id : std::to_string(qid),
+                                     intersection.length,
+                                     intersection.max_score);
         }
         qid += 1;
     }
@@ -68,7 +75,9 @@ int main(int argc, const char **argv)
     std::optional<std::string> wand_data_filename;
     std::optional<std::string> query_filename;
     std::optional<std::string> stemmer;
+    std::optional<std::uint8_t> combinations;
     bool compressed = false;
+    bool header = false;
 
     CLI::App app{"compute_intersection - a tool for pre-computing intersections of terms."};
     app.set_config("--config", "", "Configuration .ini file", false);
@@ -79,6 +88,11 @@ int main(int argc, const char **argv)
     app.add_flag("--compressed-wand", compressed, "Compressed wand input file");
     auto *terms_opt = app.add_option("--terms", terms_file, "Term lexicon");
     app.add_option("--stemmer", stemmer, "Stemmer type")->needs(terms_opt);
+    app.add_option("--combinations",
+                   combinations,
+                   "Compute intersections for all combinations of terms in query, limited by this "
+                   "number of terms");
+    app.add_flag("--header", header, "Write TSV header");
     CLI11_PARSE(app, argc, argv);
 
     std::vector<Query> queries;
@@ -90,19 +104,27 @@ int main(int argc, const char **argv)
         io::for_each_line(std::cin, parse_query);
     }
 
+    if (header) {
+        if (combinations) {
+            std::cout << "qid\tterm_mask\tlength\tmax_score\n";
+        } else {
+            std::cout << "qid\tlength\tmax_score\n";
+        }
+    }
+
     /**/
     if (false) {
-#define LOOP_BODY(R, DATA, T)                                       \
-    }                                                               \
-    else if (type == BOOST_PP_STRINGIZE(T))                         \
-    {                                                               \
-        if (compressed) {                                           \
-            intersect<BOOST_PP_CAT(T, _index), wand_uniform_index>( \
-                index_filename, wand_data_filename, queries, type); \
-        } else {                                                    \
-            intersect<BOOST_PP_CAT(T, _index), wand_raw_index>(     \
-                index_filename, wand_data_filename, queries, type); \
-        }                                                           \
+#define LOOP_BODY(R, DATA, T)                                                     \
+    }                                                                             \
+    else if (type == BOOST_PP_STRINGIZE(T))                                       \
+    {                                                                             \
+        if (compressed) {                                                         \
+            intersect<BOOST_PP_CAT(T, _index), wand_uniform_index>(               \
+                index_filename, wand_data_filename, queries, type, combinations); \
+        } else {                                                                  \
+            intersect<BOOST_PP_CAT(T, _index), wand_raw_index>(                   \
+                index_filename, wand_data_filename, queries, type, combinations); \
+        }                                                                         \
         /**/
 
         BOOST_PP_SEQ_FOR_EACH(LOOP_BODY, _, PISA_INDEX_TYPES);
