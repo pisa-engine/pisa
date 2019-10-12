df7031396cba0342ba7bda2e569c98593fbb7b9e
Antonio Mallia <me@antoniomallia.it>
2019-10-12 14:06:28 +0200

    Added sampling task

---
+++ src/sample_inverted_index.cpp
@@ -0,0 +1,73 @@
+#include <string>
+#include <random>
+#include <fstream>
+#include <cmath>
+
+#include "invert.hpp"
+
+#include "binary_freq_collection.hpp"
+
+#include "util/progress.hpp"
+
+#include "CLI/CLI.hpp"
+
+
+int main(int argc, char **argv) {
+
+    using namespace pisa;
+    std::string input_basename;
+    std::string output_basename;
+    float rate;
+
+    CLI::App app{"sample_inverted_index - a tool for sampling an inverted index."};
+    app.add_option("-c,--collection", input_basename, "Input collection basename")->required();
+    app.add_option("-o,--output", output_basename, "Output collection basename")->required();
+    app.add_option("-r,--rate", rate, "Sampling rate")->required();
+    CLI11_PARSE(app, argc, argv);
+
+    if(rate < 0 or rate > 1)
+    {
+        spdlog::error("Sampling rate should be between 0 and 1.");
+        std::abort();
+    }
+    binary_freq_collection input(input_basename.c_str());
+
+    // Copy '.sizes' file
+    std::ifstream  sis(input_basename + ".sizes");
+    std::ofstream  sos(output_basename + ".sizes");
+    sos << sis.rdbuf();
+    sis.close();
+    sos.close();
+
+    std::ofstream dos(output_basename + ".docs");
+    std::ofstream fos(output_basename + ".freqs");
+
+    auto document_count = static_cast<uint32_t>(input.num_docs());
+    write_sequence(dos, gsl::make_span<uint32_t const>(&document_count, 1));
+
+    {
+        pisa::progress progress("Sampling inverted index", input.size());
+        for (auto const &plist : input) {
+            std::vector<uint32_t> docs(plist.docs.begin(), plist.docs.end());
+            std::vector<uint32_t> freqs(plist.freqs.begin(), plist.freqs.end());
+
+            auto seed = unsigned ( std::time(0) );
+            std::mt19937 eng1(seed);
+            auto eng2 = eng1;
+            std::shuffle(docs.begin(), docs.end(), eng1);
+            std::shuffle(freqs.begin(), freqs.end(), eng2);
+            auto new_size = std::ceil(plist.docs.size() * rate);
+            docs.resize(new_size);
+            freqs.resize(new_size);
+            sort(docs.begin(), docs.end());
+            sort(freqs.begin(), freqs.end());
+            write_sequence(dos, gsl::span<uint32_t const>(docs));
+            write_sequence(fos, gsl::span<uint32_t const>(freqs));
+            progress.update(1);
+        }
+    }
+    dos.close();
+    fos.close();
+
+    return 0;
+}
