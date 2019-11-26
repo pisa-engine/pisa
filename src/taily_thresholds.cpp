083e0b44d676ddcb9a5a9134f52a60bbfce989b8
Antonio Mallia <me@antoniomallia.it>
2019-11-26 12:10:49 +0100

    First version of pairwise taily

--- src/taily_thresholds.cpp
+++ src/taily_thresholds.cpp
@@ -1,3 +1,5 @@
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/kruskal_min_spanning_tree.hpp>
 #include <iostream>
 #include <optional>
 
@@ -9,12 +11,85 @@
 
 #include "io.hpp"
 #include "query/queries.hpp"
-#include "util/util.hpp"
 #include "taily.hpp"
+#include "util/util.hpp"
 
 #include "CLI/CLI.hpp"
 
 using namespace pisa;
+using Graph = boost::adjacency_list<boost::vecS,
+                                    boost::vecS,
+                                    boost::undirectedS,
+                                    boost::no_property,
+                                    boost::property<boost::edge_weight_t, float>>;
+using Edge = boost::graph_traits<Graph>::edge_descriptor;
+
+[[nodiscard]] auto estimate_pairwise_cutoff(taily::Query_Statistics const &stats,
+                                            int ntop,
+                                            double all) -> double
+{
+    auto const dist = taily::fit_distribution(stats.term_stats);
+    double const p_c = std::min(1.0, ntop / all);
+    return boost::math::quantile(complement(dist, p_c));
+}
+
+void pairwise_thresholds(const std::string &taily_stats_filename,
+                         const std::map<std::set<uint32_t>, float> &bigrams_stats,
+                         const std::vector<Query> &queries,
+                         uint64_t k)
+{
+    std::ifstream ifs(taily_stats_filename);
+    int64_t collection_size;
+    ifs.read(reinterpret_cast<char *>(&collection_size), sizeof(collection_size));
+    int64_t term_num;
+    ifs.read(reinterpret_cast<char *>(&term_num), sizeof(term_num));
+
+    std::vector<taily::Feature_Statistics> stats;
+    for (int i = 0; i < term_num; ++i) {
+        stats.push_back(taily::Feature_Statistics::from_stream(ifs));
+    }
+
+    for (auto const &query : queries) {
+        int num_nodes = query.terms.size();
+        std::vector<std::pair<int, int>> edge_array;
+        std::vector<float> weights;
+        for (size_t i = 0; i < query.terms.size(); ++i) {
+            for (size_t j = i + 1; j < query.terms.size(); ++j) {
+                auto it = bigrams_stats.find({query.terms[i], query.terms[j]});
+                if (it != bigrams_stats.end()) {
+                    edge_array.emplace_back(i, j);
+                    weights.push_back(-(it->second));
+                }
+            }
+        }
+        Graph g(edge_array.begin(), edge_array.end(), weights.begin(), num_nodes);
+        boost::property_map<Graph, boost::edge_weight_t>::type weight = get(boost::edge_weight, g);
+        std::vector<Edge> spanning_tree;
+        boost::kruskal_minimum_spanning_tree(g, std::back_inserter(spanning_tree));
+
+        double all = 1;
+        for (std::vector<Edge>::iterator ei = spanning_tree.begin(); ei != spanning_tree.end();
+             ++ei) {
+            all *= -(weight[*ei]);
+        }
+
+        std::vector<taily::Feature_Statistics> term_stats;
+        auto terms = query.terms;
+        for (auto &&t : terms) {
+            term_stats.push_back(stats[t]);
+        }
+        taily::Query_Statistics query_stats{term_stats, collection_size};
+
+        double const any = taily::any(query_stats);
+        for (auto &&t : terms) {
+            all *= stats[t].frequency / any;
+        }
+        all *= any;
+
+        auto threshold = estimate_pairwise_cutoff(query_stats, k, all);
+        std::cout << threshold << '\n';
+    }
+}
 
 void thresholds(const std::string &taily_stats_filename,
                 const std::vector<Query> &queries,
@@ -22,21 +97,19 @@ void thresholds(const std::string &taily_stats_filename,
 {
     std::ifstream ifs(taily_stats_filename);
     int64_t collection_size;
-    ifs.read(reinterpret_cast<char*>(&collection_size), sizeof(collection_size));
+    ifs.read(reinterpret_cast<char *>(&collection_size), sizeof(collection_size));
     int64_t term_num;
-    ifs.read(reinterpret_cast<char*>(&term_num), sizeof(term_num));
+    ifs.read(reinterpret_cast<char *>(&term_num), sizeof(term_num));
 
     std::vector<taily::Feature_Statistics> stats;
-    for (int i = 0; i < term_num; ++i)
-    {
+    for (int i = 0; i < term_num; ++i) {
         stats.push_back(taily::Feature_Statistics::from_stream(ifs));
     }
-
 
     for (auto const &query : queries) {
         std::vector<taily::Feature_Statistics> term_stats;
         auto terms = query.terms;
-        for(auto&& t : terms) {
+        for (auto &&t : terms) {
             term_stats.push_back(stats[t]);
         }
         taily::Query_Statistics query_stats{term_stats, collection_size};
@@ -44,7 +117,6 @@ void thresholds(const std::string &taily_stats_filename,
         std::cout << threshold << '\n';
     }
 }
-
 
 int main(int argc, const char **argv)
 {
@@ -57,11 +129,14 @@ int main(int argc, const char **argv)
     std::optional<std::string> stemmer = std::nullopt;
     uint64_t k = configuration::get().k;
 
+    std::optional<std::string> pairwise_filename;
+
     CLI::App app{"A tool for predicting thresholds for queries using Taily."};
     app.set_config("--config", "", "Configuration .ini file", false);
     app.add_option("-t,--taily", taily_stats_filename, "Taily stats filename")->required();
     app.add_option("-q,--query", query_filename, "Queries filename");
     app.add_option("-k", k, "k value");
+    app.add_option("-p, --pairwise", pairwise_filename, "Pairwise filename");
     auto *terms_opt =
         app.add_option("--terms", terms_file, "Text file with terms in separate lines");
     app.add_option("--stemmer", stemmer, "Stemmer type")->needs(terms_opt);
@@ -76,5 +151,17 @@ int main(int argc, const char **argv)
         io::for_each_line(std::cin, parse_query);
     }
 
-    thresholds(taily_stats_filename, queries, k);
+    if (pairwise_filename) {
+        std::map<std::set<uint32_t>, float> bigrams_stats;
+        std::ifstream bigrams_fs(*pairwise_filename);
+        auto term_processor = TermProcessor(terms_file, std::nullopt, stemmer);
+        std::string line;
+        while (std::getline(bigrams_fs, line)) {
+            bigrams_stats.insert({{0, 0}, 0.0});
+        }
+
+        pairwise_thresholds(taily_stats_filename, bigrams_stats, queries, k);
+    } else {
+        thresholds(taily_stats_filename, queries, k);
+    }
 }
