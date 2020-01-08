2dc73a220205c3e1b902e10ee63c744e3e925070
Michal Siedlaczek <michal.siedlaczek@gmail.com>
2020-01-08 17:22:04 +0000

    Support posting stats

--- v1/postings.cpp
+++ v1/postings.cpp
@@ -11,6 +11,7 @@
 #include "query/queries.hpp"
 #include "topk_queue.hpp"
 #include "v1/blocked_cursor.hpp"
+#include "v1/cursor/collect.hpp"
 #include "v1/default_index_runner.hpp"
 #include "v1/index_metadata.hpp"
 #include "v1/query.hpp"
@@ -22,10 +23,8 @@
 using pisa::App;
 using pisa::Query;
 using pisa::resolve_query_parser;
+using pisa::v1::collect_payloads;
 using pisa::v1::index_runner;
-using pisa::v1::IndexMetadata;
-using pisa::v1::RawReader;
-using pisa::v1::resolve_yml;
 
 namespace arg = pisa::arg;
 
@@ -63,6 +62,67 @@ template <typename First, typename... Optional>
     return val;
 }
 
+template <typename Cursor>
+void calc_stats(Cursor&& cursor)
+{
+    using payload_type = std::decay_t<decltype(cursor.payload())>;
+    auto length = cursor.size();
+    auto payloads = collect_payloads(cursor);
+    std::sort(payloads.begin(), payloads.end(), std::greater<>{});
+    auto kth = [&](auto k) {
+        if (k < payloads.size()) {
+            return payloads[k];
+        }
+        return payload_type{};
+    };
+    std::cout << fmt::format("{}\t{}\t{}\t{}\t{}\n", length, kth(0), kth(9), kth(99), kth(999));
+}
+
+template <typename Cursor, typename Scorer>
+auto print_postings(Cursor&& cursor,
+                    Scorer&& scorer,
+                    std::optional<pisa::Payload_Vector<>> const& docmap,
+                    bool did,
+                    bool print_frequencies,
+                    bool print_scores)
+{
+    auto print = [&](auto&& cursor) {
+        if (did) {
+            std::cout << *cursor;
+        } else {
+            std::cout << docmap.value()[*cursor];
+        }
+        if (print_frequencies) {
+            std::cout << " " << cursor.payload();
+        }
+        if (print_scores) {
+            std::cout << " " << scorer(cursor.value(), cursor.payload());
+        }
+        std::cout << '\n';
+    };
+    for_each(cursor, print);
+};
+
+template <typename Cursor>
+auto print_precomputed_postings(Cursor&& cursor,
+                                std::optional<pisa::Payload_Vector<>> const& docmap,
+                                bool did)
+{
+    auto print = [&](auto&& cursor) {
+        if (did) {
+            std::cout << *cursor;
+        } else {
+            std::cout << docmap.value()[*cursor];
+        }
+        if constexpr (std::is_same_v<decltype(cursor.payload()), std::uint8_t>) {
+            std::cout << " " << static_cast<int>(cursor.payload()) << '\n';
+        } else {
+            std::cout << " " << cursor.payload() << '\n';
+        }
+    };
+    for_each(std::forward<Cursor>(cursor), print);
+};
+
 int main(int argc, char** argv)
 {
     std::optional<std::string> terms_file{};
@@ -73,6 +133,7 @@ int main(int argc, char** argv)
     bool print_frequencies = false;
     bool print_scores = false;
     bool precomputed = false;
+    bool stats = false;
 
     App<arg::Index> app{"Queries a v1 index."};
     app.add_option("--terms", terms_file, "Overrides document lexicon from .yml (if defined).");
@@ -84,6 +145,7 @@ int main(int argc, char** argv)
     app.add_flag("-f,--frequencies", print_frequencies, "Print frequencies");
     auto* scores_option = app.add_flag("-s,--scores", print_scores, "Print BM25 scores");
     app.add_flag("--precomputed", precomputed, "Use BM25 precomputed scores")->needs(scores_option);
+    app.add_flag("--stats", stats, "Print stats instead of listing postings");
     app.add_option("query", query_input, "List of terms", false)->required();
     CLI11_PARSE(app, argc, argv);
 
@@ -120,40 +182,28 @@ int main(int argc, char** argv)
         if (precomputed) {
             auto run = scored_index_runner(meta);
             run([&](auto&& index) {
-                auto print = [&](auto&& cursor) {
-                    if (did) {
-                        std::cout << *cursor;
-                    } else {
-                        std::cout << docmap.value()[*cursor];
-                    }
-                    if constexpr (std::is_same_v<decltype(cursor.payload()), std::uint8_t>) {
-                        std::cout << " " << static_cast<int>(cursor.payload()) << '\n';
-                    } else {
-                        std::cout << " " << cursor.payload() << '\n';
-                    }
-                };
-                for_each(index.cursor(query.terms.front()), print);
+                auto cursor = index.cursor(query.terms.front());
+                if (stats) {
+                    calc_stats(cursor);
+                } else {
+                    print_precomputed_postings(cursor, docmap, did);
+                }
             });
         } else {
             auto run = index_runner(meta);
             run([&](auto&& index) {
                 auto bm25 = make_bm25(index);
-                auto scorer = bm25.term_scorer(query.terms.front());
-                auto print = [&](auto&& cursor) {
-                    if (did) {
-                        std::cout << *cursor;
-                    } else {
-                        std::cout << docmap.value()[*cursor];
-                    }
-                    if (print_frequencies) {
-                        std::cout << " " << cursor.payload();
-                    }
-                    if (print_scores) {
-                        std::cout << " " << scorer(cursor.value(), cursor.payload());
-                    }
-                    std::cout << '\n';
-                };
-                for_each(index.cursor(query.terms.front()), print);
+                if (stats) {
+                    calc_stats(index.scored_cursor(query.terms.front(), bm25));
+                } else {
+                    auto scorer = bm25.term_scorer(query.terms.front());
+                    print_postings(index.cursor(query.terms.front()),
+                                   scorer,
+                                   docmap,
+                                   did,
+                                   print_frequencies,
+                                   print_scores);
+                }
             });
         }
     } else {
