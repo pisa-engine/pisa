3174b246412ed5aa8c9d5e3dba7f1e010ff1015b
Antonio Mallia <me@antoniomallia.it>
2020-01-31 07:07:04 -0500

    Missing headers

--- src/index_stats_json.cpp
+++ src/index_stats_json.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 
 #include "boost/algorithm/string/classification.hpp"
 #include "boost/algorithm/string/split.hpp"
