1a2764b506ec22fa5af37304b9efbc6072c09a17
Michał Siedlaczek <michal.siedlaczek@gmail.com>
2020-06-02 15:57:50 -0400

    Move most test to Actions (#377)

--- include/pisa/temporary_directory.hpp
+++ include/pisa/temporary_directory.hpp
@@ -5,7 +5,7 @@
 #include "boost/filesystem.hpp"
 
 struct Temporary_Directory {
-    explicit Temporary_Directory()
+    Temporary_Directory()
         : dir_(boost::filesystem::temp_directory_path() / boost::filesystem::unique_path())
     {
         if (boost::filesystem::exists(dir_)) {
@@ -14,6 +14,10 @@ struct Temporary_Directory {
         boost::filesystem::create_directory(dir_);
         std::cerr << "Created a tmp dir " << dir_.c_str() << '\n';
     }
+    Temporary_Directory(Temporary_Directory const&) = default;
+    Temporary_Directory(Temporary_Directory&&) noexcept = default;
+    Temporary_Directory& operator=(Temporary_Directory const&) = default;
+    Temporary_Directory& operator=(Temporary_Directory&&) noexcept = default;
     ~Temporary_Directory()
     {
         if (boost::filesystem::exists(dir_)) {
