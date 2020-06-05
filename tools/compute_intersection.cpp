4189626be90e99eb90bc58eb770a983211abee72
Michal Siedlaczek <michal.siedlaczek@gmail.com>
2020-06-05 16:34:31 +0000

    Fix formatting

--- tools/compute_intersection.cpp
+++ tools/compute_intersection.cpp
@@ -52,10 +52,9 @@ void intersect(
             auto intersections = nlohmann::json::array();
             auto process_intersection = [&](auto const& query, auto const& mask) {
                 auto intersection = Intersection::compute(index, wdata, query, mask);
-                intersections.push_back(nlohmann::json{
-                    {"length", intersection.length},
-                    {"max_score", intersection.max_score},
-                    {"mask", mask.to_ulong()}});
+                intersections.push_back(nlohmann::json{{"length", intersection.length},
+                                                       {"max_score", intersection.max_score},
+                                                       {"mask", mask.to_ulong()}});
             };
             for_all_subsets(query, max_term_count, process_intersection);
             auto output =
