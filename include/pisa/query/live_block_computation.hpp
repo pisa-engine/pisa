1d829ee2ca4bcc9248fd400e888e965a386f145b
Antonio Mallia <me@antoniomallia.it>
2020-06-18 19:16:29 +0200

    Added live-block computation for scalar, avx and avx2

---
+++ include/pisa/query/live_block_computation.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include "bit_vector.hpp"
+namespace pisa {
+
+bit_vector compute_live_quant16(std::vector<std::vector<uint16_t>> const& scores, uint16_t threshold)
+{
+    bit_vector_builder bv;
+    bv.reserve(scores[0].size());
+    for (size_t i = 0; i < scores[0].size(); i += 1) {
+        uint16_t sum = scores[0][i];
+        for (size_t term = 1; term < scores.size(); ++term) {
+            sum += scores[term][i];
+        }
+        bv.append_bits(sum >= threshold, 1);
+    }
+    return bit_vector(&bv);
+}
+
+bit_vector avx_compute_live_quant16(std::vector<std::vector<uint16_t>> const& scores, uint16_t threshold)
+{
+    bit_vector_builder bv;
+    bv.reserve(scores[0].size());
+    __m128i thresholds = _mm_set1_epi16(threshold);
+    size_t i = 0;
+    for (; i < scores[0].size() and scores[0].size() - i > 0; i += 8) {
+        __m128i sum = _mm_loadu_si128((__m128i*)(scores[0].data() + i));
+        for (size_t term = 1; term < scores.size(); ++term) {
+            sum = _mm_adds_epu16(sum, _mm_loadu_si128((__m128i*)(scores[term].data() + i)));
+        }
+
+        uint32_t maskBitsL1 = _mm_cmpeq_epi16_mask(_mm_max_epu16(sum, thresholds), sum);
+        bv.append_bits(maskBitsL1, 8);
+    }
+
+    auto remain = scores[0].size() - i;
+    if (remain > 0) {
+        uint32_t mask = 0;
+        for (; i < scores[0].size(); ++i) {
+            uint8_t sum = scores[0][i];
+            for (size_t term = 1; term < scores.size(); ++term) {
+                sum += scores[term][i];
+            }
+            if (sum > threshold) {
+                mask += 1;
+                mask = (mask << 1);
+            }
+        }
+        bv.append_bits(mask, remain);
+    }
+    return bit_vector(&bv);
+}
+
+bit_vector
+avx2_compute_live_quant16(std::vector<std::vector<uint16_t>> const& scores, uint16_t threshold)
+{
+    bit_vector_builder bv;
+    bv.reserve(scores[0].size());
+    __m256i thresholds = _mm256_set1_epi16(threshold);
+    size_t i = 0;
+    for (; i < scores[0].size() and scores[0].size() - i > 0; i += 16) {
+        __m256i sum = _mm256_loadu_si256((__m256i*)(scores[0].data() + i));
+        for (size_t term = 1; term < scores.size(); ++term) {
+            sum = _mm256_adds_epu16(sum, _mm256_loadu_si256((__m256i*)(scores[term].data() + i)));
+        }
+        uint32_t maskBitsL1 = _mm256_cmpeq_epi16_mask(_mm256_max_epu16(sum, thresholds), sum);
+        bv.append_bits(maskBitsL1, 16);
+    }
+
+    auto remain = scores[0].size() - i;
+    if (remain > 0) {
+        uint32_t mask = 0;
+        for (; i < scores[0].size(); ++i) {
+            uint8_t sum = scores[0][i];
+            for (size_t term = 1; term < scores.size(); ++term) {
+                sum += scores[term][i];
+            }
+            if (sum >= threshold) {
+                mask += 1;
+                mask = (mask << 1);
+            }
+        }
+        bv.append_bits(mask << (32 - remain), remain);
+    }
+    return bit_vector(&bv);
+}
+
+}  // namespace pisa
---
+++ test/test_live_block_computation.cpp
@@ -0,0 +1,58 @@
+#define CATCH_CONFIG_MAIN
+#include "bit_vector.hpp"
+#include "catch2/catch.hpp"
+#include <rapidcheck.h>
+
+#include "query/live_block_computation.hpp"
+
+using namespace pisa;
+
+TEST_CASE("test_live_block_computation")
+{
+    rc::check([](std::vector<uint16_t> scores, uint16_t threshold) {
+        {
+            auto bv = compute_live_quant16({scores}, threshold);
+            REQUIRE(bv.size() == scores.size());
+
+            bit_vector::enumerator en(bv, 0);
+            for (auto&& s: scores) {
+                bool condition = s >= threshold;
+                REQUIRE(en.next() == condition);
+            }
+        }
+    });
+}
+
+TEST_CASE("test_avx_live_block_computation")
+{
+    rc::check([](std::vector<uint16_t> scores, uint16_t threshold) {
+        {
+            auto bv = compute_live_quant16({scores}, threshold);
+            auto avx_bv = avx_compute_live_quant16({scores}, threshold);
+            REQUIRE(bv.size() == avx_bv.size());
+
+            bit_vector::enumerator en(bv, 0);
+            bit_vector::enumerator avx_en(avx_bv, 0);
+            while (en.position() < bv.size()) {
+                REQUIRE(en.next() == avx_en.next());
+            }
+        }
+    });
+}
+
+TEST_CASE("test_avx2_live_block_computation")
+{
+    rc::check([](std::vector<uint16_t> scores, uint16_t threshold) {
+        {
+            auto bv = compute_live_quant16({scores}, threshold);
+            auto avx2_bv = avx2_compute_live_quant16({scores}, threshold);
+            REQUIRE(bv.size() == avx2_bv.size());
+
+            bit_vector::enumerator en(bv, 0);
+            bit_vector::enumerator avx2_en(avx2_bv, 0);
+            while (en.position() < bv.size()) {
+                REQUIRE(en.next() == avx2_en.next());
+            }
+        }
+    });
+}
