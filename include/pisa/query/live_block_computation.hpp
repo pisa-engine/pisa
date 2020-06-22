46986c50926ca22ee87ad7a0c6f055bcbb0d4468
Antonio Mallia <me@antoniomallia.it>
2020-06-22 18:18:02 +0200

    fix clang-format

--- include/pisa/query/live_block_computation.hpp
+++ include/pisa/query/live_block_computation.hpp
@@ -31,8 +31,9 @@ bit_vector avx_compute_live_quant16(std::vector<std::vector<uint16_t>> const& sc
         }
 
         __m128i masksL1 = _mm_cmpeq_epi16(_mm_max_epu16(sum, thresholds), sum);
-        __m128i lensAll = _mm_shuffle_epi8(masksL1, _mm_setr_epi8(0, 2, 4, 6, 8, 10, 12, 14, -1, -1, -1, -1, -1, -1, -1, -1));
-		uint32_t maskBitsL1 = _mm_movemask_epi8(lensAll);
+        __m128i lensAll = _mm_shuffle_epi8(
+            masksL1, _mm_setr_epi8(0, 2, 4, 6, 8, 10, 12, 14, -1, -1, -1, -1, -1, -1, -1, -1));
+        uint32_t maskBitsL1 = _mm_movemask_epi8(lensAll);
         bv.append_bits(maskBitsL1, 8);
     }
 
@@ -69,7 +70,8 @@ avx2_compute_live_quant16(std::vector<std::vector<uint16_t>> const& scores, uint
             sum = _mm256_adds_epu16(sum, _mm256_loadu_si256((__m256i*)(scores[term].data() + i)));
         }
         __m256i masksL1 = _mm256_cmpeq_epi16(_mm256_max_epu16(sum, thresholds), sum);
-        uint32_t maskBitsL1 = _mm_movemask_epi8(_mm_packs_epi16(_mm256_extractf128_si256(masksL1, 0), _mm256_extractf128_si256(masksL1, 1)));
+        uint32_t maskBitsL1 = _mm_movemask_epi8(_mm_packs_epi16(
+            _mm256_extractf128_si256(masksL1, 0), _mm256_extractf128_si256(masksL1, 1)));
         bv.append_bits(maskBitsL1, 16);
     }
 
