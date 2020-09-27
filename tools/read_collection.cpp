42d67670a1ee455b37a9edb57790073b137419b6
Michal Siedlaczek <michal.siedlaczek@gmail.com>
2020-09-27 14:25:35 +0000

    Support reading binary collection with lexicon

--- tools/read_collection.cpp
+++ tools/read_collection.cpp
@@ -4,6 +4,9 @@
 #include <range/v3/view/iota.hpp>
 
 #include "binary_collection.hpp"
+#include "io.hpp"
+#include "memory_source.hpp"
+#include "payload_vector.hpp"
 #include "util/util.hpp"
 
 using namespace pisa;
@@ -11,47 +14,69 @@ using namespace pisa;
 int main(int argc, char** argv)
 {
     std::string collection_file;
-    std::string map_file{};
+    std::optional<std::string> map_file{};
+    std::optional<std::string> lex_file{};
     std::ptrdiff_t first, last;
 
-    CLI::App app{"read_collection - read collections."};
+    CLI::App app{"Reads binary collection to stdout."};
     app.add_option("-c,--collection", collection_file, "Collection file name")->required();
-    app.add_option("-m,--map", map_file, "String map file name");
-    app.add_option("first", first, "Element number")->required();
-    app.add_option("last", last, "Element number");
+
+    auto maptext =
+        app.add_option("--maptext", map_file, "Text file mapping each line number (ID) to a string");
+    auto maplex = app.add_option("--maplex", lex_file, "Lexicon file mapping IDs to strings");
+    maptext->excludes(maplex);
+    maplex->excludes(maptext);
+
+    app.add_option("first", first, "First element number")->required();
+    app.add_option(
+        "last",
+        last,
+        "Open-ended end of range of elements to read. "
+        "If not defined, only one element is read.");
     CLI11_PARSE(app, argc, argv);
 
-    std::function<void(std::uint32_t const&)> print = [](auto const& term) {
-        std::cout << term << " ";
-    };
+    std::function<void(std::uint32_t)> print = [](auto const& term) { std::cout << term << " "; };
 
-    std::vector<std::string> map;
-    if (not map_file.empty()) {
-        std::ifstream is(map_file);
-        std::string str;
-        while (std::getline(is, str)) {
-            map.push_back(str);
+    try {
+        if (map_file) {
+            print = [loaded_map = pisa::io::read_string_vector(*map_file)](std::uint32_t term) {
+                std::cout << loaded_map.at(term) << ' ';
+            };
         }
-        print = [&](std::uint32_t const& term) { std::cout << map[term] << " "; };
-    }
 
-    binary_collection coll(collection_file.c_str());
-    auto iter = coll.begin();
-    for ([[maybe_unused]] auto idx: ranges::views::iota(0, first)) {
-        ++iter;
-    }
-
-    if (app.count("last") == 0U) {
-        last = first + 1;
-    }
-
-    for (; first < last; ++first) {
-        auto sequence = *iter;
-        for (auto const& term: sequence) {
-            print(term);
+        if (lex_file) {
+            auto source =
+                std::make_shared<pisa::MemorySource>(pisa::MemorySource::mapped_file(*lex_file));
+            auto lexicon = Payload_Vector<>::from(*source);
+            print = [source = std::move(source), lexicon](std::uint32_t term) {
+                std::cout << lexicon[term] << ' ';
+            };
         }
-        std::cout << '\n';
-        ++iter;
+
+        binary_collection coll(collection_file.c_str());
+        auto iter = coll.begin();
+        for ([[maybe_unused]] auto idx: ranges::views::iota(0, first)) {
+            ++iter;
+        }
+
+        if (app.count("last") == 0U) {
+            last = first + 1;
+        }
+
+        for (; first < last; ++first) {
+            auto sequence = *iter;
+            for (auto term: sequence) {
+                print(term);
+            }
+            std::cout << '\n';
+            ++iter;
+        }
+    } catch (std::exception const& err) {
+        spdlog::error("{}", err.what());
+        return 1;
+    } catch (...) {
+        spdlog::error("Unknown error");
+        return 1;
     }
 
     return 0;
