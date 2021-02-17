21f2e009017f8d24076bfc1e46926baa30c386d6
Michał Siedlaczek <michal.siedlaczek@gmail.com>
2021-02-16 21:45:51 -0500

    Enable counting postings by term (#453)
    Enable counting postings by term.

--- tools/count_postings.cpp
+++ tools/count_postings.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 
 #include <CLI/CLI.hpp>
+#include <boost/algorithm/string/join.hpp>
+#include <boost/range/adaptor/transformed.hpp>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
@@ -13,13 +15,38 @@
 using namespace pisa;
 
 template <typename Index>
-void extract(std::string const& index_filename, std::vector<pisa::Query> const& queries)
+void extract(
+    std::string const& index_filename,
+    std::vector<pisa::Query> const& queries,
+    std::string const& separator,
+    bool sum,
+    bool print_qid)
 {
     Index index(MemorySource::mapped_file(index_filename));
-    auto accumulate_size = [&](auto sum, auto term_id) { return sum + index[term_id].size(); };
+    auto body = [&] {
+        if (sum) {
+            return std::function<void(Query const&)>([&](auto const& query) {
+                auto count = std::accumulate(
+                    query.terms.begin(), query.terms.end(), 0, [&](auto s, auto term_id) {
+                        return s + index[term_id].size();
+                    });
+                std::cout << count << '\n';
+            });
+        }
+        return std::function<void(Query const&)>([&](auto const& query) {
+            std::cout << boost::algorithm::join(
+                query.terms | boost::adaptors::transformed([&index](auto term_id) {
+                    return std::to_string(index[term_id].size());
+                }),
+                separator);
+            std::cout << '\n';
+        });
+    }();
     for (auto const& query: queries) {
-        auto count = std::accumulate(query.terms.begin(), query.terms.end(), 0, accumulate_size);
-        std::cout << count << '\n';
+        if (print_qid && query.id) {
+            std::cout << *query.id << ":";
+        }
+        body(query);
     }
 }
 
@@ -28,17 +55,25 @@ int main(int argc, char** argv)
     spdlog::drop("");
     spdlog::set_default_logger(spdlog::stderr_color_mt(""));
 
-    App<arg::Index, arg::Query<arg::QueryMode::Unranked>> app{
+    bool sum = false;
+
+    App<arg::Index, arg::Query<arg::QueryMode::Unranked>, arg::Separator, arg::PrintQueryId> app{
         "Extracts posting counts from an inverted index."};
+    app.add_flag(
+        "--sum",
+        sum,
+        "Sum postings accross the query terms; by default, individual list lengths will be "
+        "printed, separated by the separator defined with --sep");
     CLI11_PARSE(app, argc, argv);
 
     /**/
     if (false) {
-#define LOOP_BODY(R, DATA, T)                                                  \
-    }                                                                          \
-    else if (app.index_encoding() == BOOST_PP_STRINGIZE(T))                    \
-    {                                                                          \
-        extract<BOOST_PP_CAT(T, _index)>(app.index_filename(), app.queries()); \
+#define LOOP_BODY(R, DATA, T)                                                                 \
+    }                                                                                         \
+    else if (app.index_encoding() == BOOST_PP_STRINGIZE(T))                                   \
+    {                                                                                         \
+        extract<BOOST_PP_CAT(T, _index)>(                                                     \
+            app.index_filename(), app.queries(), app.separator(), sum, app.print_query_id()); \
         /**/
         BOOST_PP_SEQ_FOR_EACH(LOOP_BODY, _, PISA_INDEX_TYPES);
 #undef LOOP_BODY
--- tools/map_queries.cpp
+++ tools/map_queries.cpp
@@ -14,22 +14,18 @@ int main(int argc, const char** argv)
     spdlog::drop("");
     spdlog::set_default_logger(spdlog::stderr_color_mt(""));
 
-    std::string separator = "\t";
-    bool query_id = false;
-
-    App<arg::Query<arg::QueryMode::Unranked>> app{
+    App<arg::Query<arg::QueryMode::Unranked>, arg::Separator, arg::PrintQueryId> app{
         "A tool for transforming textual queries to IDs."};
-    app.add_option("--sep", separator, "Separator");
-    app.add_flag("--query-id", query_id, "Print query ID (as id:T1 T2 ... TN)");
     CLI11_PARSE(app, argc, argv);
 
     using boost::adaptors::transformed;
     using boost::algorithm::join;
     for (auto&& q: app.queries()) {
-        if (query_id and q.id) {
+        if (app.print_query_id() and q.id) {
             std::cout << *(q.id) << ":";
         }
-        std::cout << join(q.terms | transformed([](auto d) { return std::to_string(d); }), separator)
-                  << '\n';
+        std::cout
+            << join(q.terms | transformed([](auto d) { return std::to_string(d); }), app.separator())
+            << '\n';
     }
 }
