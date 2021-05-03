2fb0d30cde3193c2f9ae3d6207095a4715ed5e68
Michal Siedlaczek <siedlaczek@pm.me>
2021-05-03 10:26:53 -0400

    Document type index code

--- include/pisa/index_types.hpp
+++ include/pisa/index_types.hpp
@@ -26,12 +26,9 @@
 namespace pisa {
 
 using ef_index = freq_index<compact_elias_fano, positive_sequence<strict_elias_fano>>;
-
 using single_index = freq_index<indexed_sequence, positive_sequence<>>;
-
 using pefuniform_index =
     freq_index<uniform_partitioned_sequence<>, positive_sequence<uniform_partitioned_sequence<strict_sequence>>>;
-
 using pefopt_index =
     freq_index<partitioned_sequence<>, positive_sequence<partitioned_sequence<strict_sequence>>>;
 
@@ -57,6 +54,7 @@ using profiling_block_simple8b_index = block_freq_index<pisa::simple8b_block, tr
 using profiling_block_simple16_index = block_freq_index<pisa::simple16_block, true>;
 using profiling_block_simdbp_index = block_freq_index<pisa::simdbp_block, true>;
 
+/// Exception thrown when an invalid index encoding is requested. See `IndexType::resolve`.
 class InvalidEncoding: std::exception {
   public:
     explicit InvalidEncoding(std::string_view encoding)
@@ -70,11 +68,16 @@ class InvalidEncoding: std::exception {
 
 namespace detail {
 
+    /// This object holds an index type; this can be passed to a visitor without constructing the
+    /// index itself.
     template <typename Index>
     struct IndexTypeMarker {
         using type = Index;
     };
 
+    /// Variant type listing all supported index types.
+    ///
+    /// NOTE: To support a new index type in the tools, it must be added here.
     using IndexType = std::variant<
         IndexTypeMarker<ef_index>,
         IndexTypeMarker<single_index>,
@@ -101,6 +104,14 @@ namespace detail {
         IndexTypeMarker<profiling_block_simple16_index>,
         IndexTypeMarker<profiling_block_simdbp_index>>;
 
+}  // namespace detail
+
+/// Number of supported index types.
+constexpr std::size_t NUM_INDEX_TYPES = variant_size_v<detail::IndexType>;
+
+namespace detail {
+
+    /// Returns the string representation of the index type `I`.
     template <typename I>
     [[nodiscard]] constexpr auto index_name() noexcept -> std::string_view
     {
@@ -165,6 +176,12 @@ namespace detail {
         using type = block_freq_index<Block, true>;
     };
 
+    /// Attempts to match the given encoding name to the index type at position `N` in the index
+    /// type variant.
+    ///
+    /// The function passed in the argument assigns the resolved type object if it is matched with
+    /// the encoding. If the match succeeds, this function returns `true`, otherwise, it returns
+    /// `false`.
     template <bool Profile, std::size_t N, typename Fn>
     auto resolve_index_n(std::string_view encoding, Fn&& fn) -> bool
     {
@@ -182,6 +199,8 @@ namespace detail {
         return false;
     }
 
+    /// Returns the type object of the index with the given encoding, or throws `InvalidEncoding` if
+    /// the given encoding name does not match any index type.
     template <bool Profile, std::size_t... N>
     auto resolve_index_type(std::string_view encoding, std::integer_sequence<std::size_t, N...>)
         -> IndexType
@@ -195,6 +214,8 @@ namespace detail {
         return index_type;
     }
 
+    /// Returns the type object of the index with the given encoding, or throws `InvalidEncoding` if
+    /// the given encoding name does not match any index type.
     template <bool Profile>
     [[nodiscard]] auto resolve_index_type(std::string_view encoding) -> IndexType
     {
@@ -203,52 +224,78 @@ namespace detail {
         return detail::resolve_index_type<Profile>(encoding, std::make_index_sequence<num_types>{});
     }
 
+    template <std::size_t I>
+    constexpr void push_encoding(std::array<std::string_view, NUM_INDEX_TYPES>& encodings)
+    {
+        encodings[I] =
+            detail::index_name<typename std::variant_alternative_t<I, detail::IndexType>::type>();
+    }
+
+    template <std::size_t... I>
+    [[nodiscard]] constexpr auto encodings(std::integer_sequence<std::size_t, I...>)
+        -> std::array<std::string_view, NUM_INDEX_TYPES>
+    {
+        std::array<std::string_view, NUM_INDEX_TYPES> encodings;
+        (push_encoding<I>(encodings), ...);
+        return encodings;
+    }
+
 };  // namespace detail
 
-using Encodings = std::array<std::string_view, variant_size_v<detail::IndexType>>;
-
-template <std::size_t I>
-constexpr void push_encoding(Encodings& encodings)
+/// Returns an array of the names of all supported index types.
+[[nodiscard]] inline constexpr auto encodings() -> std::array<std::string_view, NUM_INDEX_TYPES>
 {
-    encodings[I] =
-        detail::index_name<typename std::variant_alternative_t<I, detail::IndexType>::type>();
+    return detail::encodings(std::make_index_sequence<variant_size_v<detail::IndexType>>{});
 }
 
-template <std::size_t... I>
-[[nodiscard]] constexpr auto encodings(std::integer_sequence<std::size_t, I...>) -> Encodings
-{
-    Encodings encodings;
-    (push_encoding<I>(encodings), ...);
-    return encodings;
-}
-
-[[nodiscard]] inline constexpr auto encodings() -> Encodings
-{
-    return encodings(std::make_index_sequence<variant_size_v<detail::IndexType>>{});
-}
-
+/// Represents an index type.
+///
+/// This type's objects are used execute monomorphized code on an index of a particular type.
+/// See `execute()` and `load_and_execute()` for more details.
 class IndexType {
     detail::IndexType m_type_marker;
 
     explicit IndexType(detail::IndexType type_marker) : m_type_marker(type_marker) {}
 
   public:
+    /// Resolves the index type beased on its string representation.
     static auto resolve(std::string_view encoding) -> IndexType
     {
         return IndexType(detail::resolve_index_type<false>(encoding));
     }
 
+    /// Same as `resolve` but use a profiling index.
     static auto resolve_profiling(std::string_view encoding) -> IndexType
     {
         return IndexType(detail::resolve_index_type<true>(encoding));
     }
 
+    /// Executes a templated `fn`, passing to it an index type marker that can be used to access the
+    /// index type.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// IndexType::resolve("block_simdbp").execute([](auto marker) {
+    ///     using index_type = typename decltype(marker)::type;
+    /// })
+    /// ```
     template <typename Fn>
     void execute(Fn fn)
     {
         std::visit(fn, m_type_marker);
     }
 
+    /// Loads an index from the given path, and executes a templated `fn`, passing to it the loaded
+    /// index.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// IndexType::resolve("block_simdbp").load_and_execute(index_path, [](auto&& index) {
+    ///     // use `index`, which ehere is of type `block_simdbp_index`
+    /// })
+    /// ```
     template <typename Fn>
     void load_and_execute(std::string const& index_path, Fn fn)
     {
