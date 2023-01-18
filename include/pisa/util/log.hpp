119bddedcba37b4958e5e629c4df0a04a7ae65f6
Michal Siedlaczek <michal@siedlaczek.me>
2023-01-18 18:44:34 -0500

    Do not calculate log2(0)
    Instead, when precomputing values for pisa::Log2<N>, we explicitly set
    the value for 0 as -inf.

--- include/pisa/util/log.hpp
+++ include/pisa/util/log.hpp
@@ -2,21 +2,23 @@
 
 #include <array>
 #include <cmath>
+#include <limits>
 
 namespace pisa {
 
-template <size_t N>
+template <std::size_t N>
 class Log2 {
-    static_assert(N >= 0, "number of precomputed values must be non-negative");
+    static_assert(N >= 1, "number of precomputed values must be positive");
 
   public:
     constexpr Log2()
     {
-        for (size_t n = 0; n < N; ++n) {
+        m_values[0] = -std::numeric_limits<double>::infinity();
+        for (std::size_t n = 1; n < N; ++n) {
             m_values[n] = std::log2(n);
         }
     }
-    constexpr double operator()(size_t n) const
+    constexpr double operator()(std::size_t n) const
     {
         if (n >= m_values.size()) {
             return std::log2(n);
