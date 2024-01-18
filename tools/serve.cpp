6a6b320ed7426b889b03af56d68a9b9e6c6ac350
Antonio Mallia <antonio@pinecone.io>
2024-01-18 22:27:04 +0000

    Adding suport for taat

--- tools/serve.cpp
+++ tools/serve.cpp
@@ -1,15 +1,3 @@
-#include <range/v3/view/enumerate.hpp>
-#include <boost/asio.hpp>
-#include <boost/beast.hpp>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
-#include <iostream>
-#include <memory>
-#include <string>
-#include <thread>
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/spdlog.h>
-#include <CLI/CLI.hpp>
 #include "accumulator/lazy_accumulator.hpp"
 #include "accumulator/simple_accumulator.hpp"
 #include "app.hpp"
@@ -18,14 +6,27 @@
 #include "cursor/scored_cursor.hpp"
 #include "index_types.hpp"
 #include "io.hpp"
+#include "pisa/text_analyzer.hpp"
 #include "query/algorithm.hpp"
+#include "query/algorithm/ranked_or_taat_query.hpp"
+#include "query/queries.hpp"
 #include "scorer/scorer.hpp"
+#include "tokenizer.hpp"
 #include "util/util.hpp"
 #include "wand_data_compressed.hpp"
 #include "wand_data_raw.hpp"
-#include "pisa/text_analyzer.hpp"
-#include "query/queries.hpp"
-#include "tokenizer.hpp"
+#include <CLI/CLI.hpp>
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <iostream>
+#include <memory>
+#include <range/v3/view/enumerate.hpp>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+#include <string>
+#include <thread>
 
 using namespace pisa;
 using ranges::views::enumerate;
@@ -47,7 +48,6 @@ void handle_request(http::request<http::string_body> const& req, http::response<
         boost::property_tree::read_json(is, pt);
         query = pt.get<std::string>("query", "");
         k = pt.get<size_t>("k");
-
     }
 
     auto search_results = g_query_fun(query, k);
@@ -56,7 +56,7 @@ void handle_request(http::request<http::string_body> const& req, http::response<
     pt.put("query", query);
     boost::property_tree::ptree results;
 
-    for (const auto& result : search_results) {
+    for (const auto& result: search_results) {
         results.put(std::get<0>(result), std::get<1>(result));
     }
     pt.add_child("results", results);
@@ -71,29 +71,34 @@ void handle_request(http::request<http::string_body> const& req, http::response<
     res.prepare_payload();
 }
 
-class Session : public std::enable_shared_from_this<Session> {
-public:
+class Session: public std::enable_shared_from_this<Session> {
+  public:
     Session(tcp::socket socket) : socket_(std::move(socket)) {}
     void start() { read(); }
-private:
+
+  private:
     tcp::socket socket_;
     beast::flat_buffer buffer_;
     http::request<http::string_body> req_;
     http::response<http::string_body> res_;
 
-    void read() {
+    void read()
+    {
         auto self = shared_from_this();
         http::async_read(socket_, buffer_, req_, [self](beast::error_code ec, std::size_t) {
-            if (!ec) self->process_request();
+            if (!ec)
+                self->process_request();
         });
     }
 
-    void process_request() {
+    void process_request()
+    {
         handle_request(req_, res_);
         write();
     }
 
-    void write() {
+    void write()
+    {
         auto self = shared_from_this();
         http::async_write(socket_, res_, [self](beast::error_code ec, std::size_t) {
             self->socket_.shutdown(tcp::socket::shutdown_send, ec);
@@ -102,21 +107,20 @@ private:
 };
 
 class Server {
-public:
-    Server(asio::io_context& ioc, tcp::endpoint endpoint) : acceptor_(ioc, endpoint) {
-        accept();
-    }
+  public:
+    Server(asio::io_context& ioc, tcp::endpoint endpoint) : acceptor_(ioc, endpoint) { accept(); }
 
-private:
+  private:
     tcp::acceptor acceptor_;
-    void accept() {
+    void accept()
+    {
         acceptor_.async_accept([this](beast::error_code ec, tcp::socket socket) {
-            if (!ec) std::make_shared<Session>(std::move(socket))->start();
+            if (!ec)
+                std::make_shared<Session>(std::move(socket))->start();
             accept();
         });
     }
 };
-
 
 template <typename IndexType, typename WandType>
 void prepare_handle_request(
@@ -128,12 +132,9 @@ void prepare_handle_request(
     TermProcessor& term_processor,
     std::string const& _tokenizer,
     std::unique_ptr<index_scorer<WandType>>& scorer,
-    const bool weighted) {
-
-
+    const bool weighted)
+{
     WhitespaceTokenizer tokenizer;
-
-
 
     if (query_type == "wand") {
         g_query_fun = [&, tokenizer](std::string& line, size_t k) {
@@ -143,7 +144,7 @@ void prepare_handle_request(
             wand_q(make_max_scored_cursors(index, wdata, *scorer, query, weighted), index.num_docs());
             topk.finalize();
 
-            spdlog::info("Query: {}",topk.size());
+            spdlog::info("Query: {}", topk.size());
             std::vector<std::tuple<std::string, float>> output;
             for (auto&& [rank, result]: enumerate(topk.topk())) {
                 output.push_back(std::make_tuple(std::string(docmap[result.second]), result.first));
@@ -193,7 +194,7 @@ void prepare_handle_request(
             for (auto&& [rank, result]: enumerate(topk.topk())) {
                 output.push_back(std::make_tuple(std::string(docmap[result.second]), result.first));
             }
-            return output;        
+            return output;
         };
     } else if (query_type == "ranked_and") {
         g_query_fun = [&, tokenizer](std::string& line, size_t k) {
@@ -208,6 +209,17 @@ void prepare_handle_request(
                 output.push_back(std::make_tuple(std::string(docmap[result.second]), result.first));
             }
             return output;
+        };
+    } else if (query_type == "ranked_or_taat") {
+        SimpleAccumulator accumulator(index.num_docs());
+        topk_queue topk(k);
+        query_fun = [&, topk, accumulator](Query query, Score threshold) mutable {
+            ranked_or_taat_query ranked_or_taat_q(topk);
+            topk.clear(threshold);
+            ranked_or_taat_q(
+                make_scored_cursors(index, *scorer, query, weighted), index.num_docs(), accumulator);
+            topk.finalize();
+            return topk.topk().size();
         };
     } else if (query_type == "ranked_or") {
         g_query_fun = [&, tokenizer](std::string& line, size_t k) {
@@ -239,30 +251,26 @@ void prepare_handle_request(
     } else {
         spdlog::error("Unsupported query type: {}", query_type);
     }
-
 }
 
 using wand_raw_index = wand_data<wand_data_raw>;
 using wand_uniform_index = wand_data<wand_data_compressed<>>;
 using wand_uniform_index_quantized = wand_data<wand_data_compressed<PayloadType::Quantized>>;
 
-int main(int argc, const char** argv) {
+int main(int argc, const char** argv)
+{
     spdlog::set_default_logger(spdlog::stderr_color_mt("default"));
     std::string documents_file;
     bool weighted = false;
-    std::string  term_lexicon;
-    std::string  tokenizer;
+    std::string term_lexicon;
+    std::string tokenizer;
     std::string ip = "0.0.0.0";
     unsigned short port = 8080;
     bool quantized = false;
     const std::set<std::string> VALID_TOKENIZERS = {"whitespace", "english"};
 
-    App<arg::Index,
-        arg::WandData<arg::WandMode::Required>,
-        arg::Algorithm,
-        arg::Scorer,
-        arg::LogLevel>
-        app{"HTTP endpoint to retrieve query results."};
+    App<arg::Index, arg::WandData<arg::WandMode::Required>, arg::Algorithm, arg::Scorer, arg::LogLevel> app{
+        "HTTP endpoint to retrieve query results."};
     app.add_option("--documents", documents_file, "Document lexicon")->required();
     app.add_flag("--quantized", quantized, "Quantized scores");
     app.add_option("--terms", term_lexicon, "Term lexicon")->required();
@@ -279,12 +287,21 @@ int main(int argc, const char** argv) {
     auto source = std::make_shared<mio::mmap_source>(documents_file.c_str());
     auto docmap = Payload_Vector<std::string_view>::from(*source);
 
-    block_simdbp_index index(MemorySource::mapped_file(app.index_filename()));                       
-    wand_raw_index wdata(MemorySource::mapped_file(app.wand_data_path()));                        
-    auto scorer = scorer::from_params(app.scorer_params(), wdata);                                
-    prepare_handle_request<block_simdbp_index, wand_raw_index>( index, wdata, app.index_encoding(), app.algorithm(),docmap,term_processor,tokenizer, scorer,weighted);  
+    block_simdbp_index index(MemorySource::mapped_file(app.index_filename()));
+    wand_raw_index wdata(MemorySource::mapped_file(app.wand_data_path()));
+    auto scorer = scorer::from_params(app.scorer_params(), wdata);
+    prepare_handle_request<block_simdbp_index, wand_raw_index>(
+        index,
+        wdata,
+        app.index_encoding(),
+        app.algorithm(),
+        docmap,
+        term_processor,
+        tokenizer,
+        scorer,
+        weighted);
 
-    spdlog::info("Starting the server. IP: {}, port: {}",ip, port);
+    spdlog::info("Starting the server. IP: {}, port: {}", ip, port);
 
     auto address = asio::ip::make_address(ip);
     asio::io_context ioc{1};
@@ -296,7 +313,7 @@ int main(int argc, const char** argv) {
     for (int i = 0; i < std::thread::hardware_concurrency(); ++i) {
         thread_pool.emplace_back([&ioc] { ioc.run(); });
     }
-    for (auto& t : thread_pool) {
+    for (auto& t: thread_pool) {
         t.join();
     }
 }
