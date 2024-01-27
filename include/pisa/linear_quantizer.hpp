ebb6fef2fa0a339e4311e3d4fd5e12b8d37f5426
Michal Siedlaczek <michal@siedlaczek.me>
2024-01-26 22:10:57 -0500

    Adjust linear quantizer
    Make sure that the max value after quantization fits in the given number
    of bits.

--- include/pisa/linear_quantizer.hpp
+++ include/pisa/linear_quantizer.hpp
@@ -1,19 +1,23 @@
 #pragma once
-#include "spdlog/spdlog.h"
+
 #include <cmath>
+#include <cstdint>
+
+#include <fmt/format.h>
 #include <gsl/gsl_assert>
 
 namespace pisa {
 
 struct LinearQuantizer {
-    explicit LinearQuantizer(float max, uint8_t bits)
-        : m_max(max), m_scale(static_cast<float>(1U << (bits)) / max) {
+    explicit LinearQuantizer(float max, std::uint8_t bits)
+        : m_max(max), m_scale(static_cast<float>((1U << bits) - 1U) / max) {
         if (bits > 32 or bits == 0) {
             throw std::runtime_error(fmt::format(
                 "Linear quantizer must take a number of bits between 1 and 32 but {} passed", bits
             ));
         }
     }
+
     [[nodiscard]] auto operator()(float value) const -> std::uint32_t {
         Expects(value <= m_max);
         return std::ceil(value * m_scale);
