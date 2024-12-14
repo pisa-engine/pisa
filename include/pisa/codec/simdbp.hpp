763be9ffa3d4c5132b8228f5fcc15a5a2d5713f6
Michal Siedlaczek <michal@siedlaczek.me>
2024-12-14 09:52:18 -0500

    Add virtual constructor to block codecs
    We construct codecs as a shared pointer to `BlockCodec` and without a
    virtual destructor, only the destructor of the base class is called.
    This has not caused any real issues because the codecs do not do any
    memory management, but this is now for correctness and future-proofing.
    
    Signed-off-by: Michal Siedlaczek <michal@siedlaczek.me>

--- include/pisa/codec/simdbp.hpp
+++ include/pisa/codec/simdbp.hpp
@@ -18,6 +18,8 @@ class SimdBpBlockCodec: public BlockCodec {
   public:
     constexpr static std::string_view name = "block_simdbp";
 
+    virtual ~SimdBpBlockCodec() = default;
+
     void encode(uint32_t const* in, uint32_t sum_of_values, size_t n, std::vector<uint8_t>& out)
         const override;
     uint8_t const*
