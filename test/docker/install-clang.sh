d0145556881e23e9b3ac53441c69675209ba0a76
Luke Gallagher <lgrz@users.noreply.github.com>
2026-09-21 01:23:37 +1000

    Fix CI-build, dockerfiles (#643)
    * Fix CI-build
    
    The gcc images have moved on from bullseye and for clang,
    `install_clang.sh` is already tied to apt sources for bookworm.
    
    This is a minimum fix to get builds working. Future work/discussion should
    consider moving to trixie and more recent gcc/clang images.
    This would also be a good time to consolidate the build pipeline.
    
    Fix the ca-cert problem by using `llvm.sh` instead of direct APT source
    URIs.
    
    * Add llvm snapshot key
    
    - Key file cannot be fetched during remote build
    - Key added from https://apt.llvm.org/llvm-snapshot.gpg.key
    
    * Change version in actions/checkout
    
    This sets `pr.yml`, `trunk.yml` to checkout@v6 which aligns them with
    the version used in `commit.yml`. This should also fix the remote build
    deprecation warnings.

--- test/docker/install-clang.sh
+++ test/docker/install-clang.sh
@@ -9,29 +9,16 @@ set -e
 
 version="$1"
 shift
-sources_file='/etc/apt/sources.list.d/llvm.list'
-
-echo 'deb http://apt.llvm.org/bullseye/ llvm-toolchain-bullseye main' > "$sources_file"
-echo 'deb-src http://apt.llvm.org/bullseye/ llvm-toolchain-bullseye main' >> "$sources_file"
-echo "deb http://apt.llvm.org/bullseye/ llvm-toolchain-bullseye-$version main" >> "$sources_file"
-echo "deb-src http://apt.llvm.org/bullseye/ llvm-toolchain-bullseye-$version main" >> "$sources_file"
 
 apt-get update
-apt-get -y install wget sudo gnupg cmake libtool git
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-apt-get update
-apt-get -y install \
-    "clang-$version" \
-    "lldb-$version" \
-    "lld-$version" \
-    "libc++-$version-dev" \
-    "libc++abi-$version-dev" \
-    "libunwind-$version-dev" "$@"
+apt-get -y install lsb-release software-properties-common wget curl sudo gnupg cmake libtool git ca-certificates
+/llvm.sh "$version" all
+
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
 # find "/usr/lib/llvm-$version/" -type f
-for f in /usr/lib/llvm-$version/bin/*
+for f in /usr/lib/llvm-${version}/bin/*
 do
     ln -sf "$f" /usr/bin
 done
